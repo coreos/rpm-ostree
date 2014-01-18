84d6f10495c760a9c17a834beeb2a1ebadce9b54
Colin Walters <walters@verbum.org>
2014-01-18 16:29:20 -0500

    autobuilder: Implement basic autobuilder which does build every 3 hours

--- src/autobuilder/js/builtins/autobuilder.js
+++ src/autobuilder/js/builtins/autobuilder.js
@@ -28,25 +28,14 @@ const BuildUtil = imports.buildutil;
 const ProcUtil = imports.procutil;
 const VersionedDir = imports.versioneddir;
 
-var AutoBuilderIface = '<node> \
-<interface name="org.gnome.OSTreeBuild.AutoBuilder"> \
-<method name="queueResolve"> \
-    <arg type="as" direction="in" /> \
-</method> \
-<property name="Status" type="s" access="read" /> \
-</interface> \
-</node>';
-
 const Autobuilder = new Lang.Class({
     Name: 'Autobuilder',
     Extends: Builtin.Builtin,
 
-    DESCRIPTION: "Automatically fetch git repositories and build",
+    DESCRIPTION: "Trigger builds every 3 hours",
 
     _init: function() {
 	this.parent();
-
-        this.parser.addArgument('--autoupdate-self', { action: 'store' });
 
 	this._buildNeeded = true;
 	this._initialResolveNeeded = true;
@@ -63,27 +52,15 @@ const Autobuilder = new Lang.Class({
         this._resultsDir = this.workdir.get_child('results');
         GSystem.file_ensure_directory(this._resultsDir, true, cancellable);
 
-	if (args.autoupdate_self)
-	    this._autoupdate_self = Gio.File.new_for_path(args.autoupdate_self);
-
-	this._ownId = Gio.DBus.session.own_name('org.gnome.OSTreeBuild', Gio.BusNameOwnerFlags.NONE,
-						function(name) {},
-						function(name) { loop.quit(); });
-
-	this._impl = Gio.DBusExportedObject.wrapJSObject(AutoBuilderIface, this);
-	this._impl.export(Gio.DBus.session, '/org/gnome/OSTreeBuild/AutoBuilder');
-
 	this._taskmaster = new Task.TaskMaster(this.workdir);
 	this._taskmaster.connect('task-executing', Lang.bind(this, this._onTaskExecuting));
 	this._taskmaster.connect('task-complete', Lang.bind(this, this._onTaskCompleted));
 
-	/* Start an initial, non-fetching resolve */
-	this._runResolve();
-	/* Flag immediately that we need a full resolve */
-	this._fullResolveNeeded = true;
-	/* And set a timeout for 10 minutes for the next full resolve */
-	this._resolveTimeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT,
-							 60 * 10, Lang.bind(this, this._triggerFullResolve));
+	// Build every 3 hours
+	this._buildTimeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT,
+						      60 * 60 * 3,
+						      Lang.bind(this, this._triggerBuild));
+	this._triggerBuild();
 
 	this._updateStatus();
 
@@ -150,59 +127,17 @@ const Autobuilder = new Lang.Class({
 	    JsonUtil.writeJsonFileAtomic(statusPath, {'running': runningTasks,
 						      'queued': queuedTasks,
 						      'systemLoad': loadAvg}, null);
-	    this._impl.emit_property_changed('Status', new GLib.Variant("s", this._status));
 	}
     },
 
-    get Status() {
-	return this._status;
-    },
-
-    queueResolve: function(srcUrls) {
-	print("Queuing force resolve for " + JSON.stringify(srcUrls));
-        this._resolveSrcUrls.push.apply(this._resolveSrcUrls, srcUrls);
-        this._runResolve();
-    },
-
-    _triggerFullResolve: function() {
-	this._fullResolveNeeded = true;
-	this._runResolve();
-	return true;
-    },
-
-    _runResolve: function() {
+    _triggerBuild: function() {
 	let cancellable = null;
 	
-	if (!(this._initialResolveNeeded ||
-	      this._resolveSrcUrls.length > 0 ||
-	      this._fullResolveNeeded))
+	if (this._taskmaster.isTaskQueued('build'))
 	    return;
 
-	if (this._taskmaster.isTaskQueued('resolve'))
-	    return;
-
-	if (this._autoupdate_self)
-	    ProcUtil.runSync(['git', 'pull', '-r'], cancellable,
-			     { cwd: this._autoupdate_self })
-
-	let previousVersion = this._buildsDir.currentVersion(cancellable);
-        let buildPath = this._buildsDir.allocateNewVersion(cancellable);
-        let version = this._buildsDir.pathToVersion(buildPath);
-        if (previousVersion) {
-            let lastBuildPath = this._buildsDir.createPathForVersion(previousVersion);
-            BuildUtil.atomicSymlinkSwap(buildPath.get_child('last-build'), lastBuildPath, null);
-        }
-
-	if (this._initialResolveNeeded) {
-	    this._initialResolveNeeded = false;
-	    this._taskmaster.pushTask(buildPath, 'resolve', { });
-	} else if (this._fullResolveNeeded) {
-	    this._fullResolveNeeded = false;
-	    this._taskmaster.pushTask(buildPath, 'resolve', { fetchAll: true });
-	} else {
-	    this._taskmaster.pushTask(buildPath, 'resolve', { fetchSrcUrls: this._resolveSrcUrls });
-	}
-	this._resolveSrcUrls = [];
+	let buildPath = this._buildsDir.allocateNewVersion(cancellable);
+	this._taskmaster.pushTask(buildPath, 'build', { });
 
 	this._updateStatus();
     }
