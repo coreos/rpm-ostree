f7ad94d8084f163ac506e05b94a57376b43471bb
Anne LoVerso <aelv13@gmail.com>
2014-06-12 10:32:21 -0400

    status: Rework code organization and readability
    This includes several changes to format the code to follow GNU Coding Standards as per
    the restof rpm-ostree.  This includes fixing indentations, adding spaces between
    identifers and parentheses, replacing "goto print" with an else structure, and
    reformatting print statements into complete sentences for internationalization.

--- src/rpmostree-builtin-status.c
+++ src/rpmostree-builtin-status.c
@@ -1,6 +1,6 @@
 /* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
  *
- * Copyright (C) 2014 Colin Walters <walters@verbum.org>
+ * Copyright (C) 2014 Anne LoVerso <anne.loverso@students.olin.edu>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -39,9 +39,9 @@ rpmostree_builtin_status (int             argc,
   gs_unref_ptrarray GPtrArray *deployments = NULL;  // list of all depoyments
   OstreeDeployment *booted_deployment = NULL;   // current booted deployment
   GOptionContext *context = g_option_context_new ("- Get the version of the booted system");
-  char *default_boot_message = NULL;    // is this deployment default? either "" or " not"
-  char *default_instruct = NULL;       // how to use rollback to change defauly; depending on state
-  char *version_message = NULL;        // is this the most recent version?  either "" or " not"
+  char *default_boot_message = NULL;    // is this deployment default? 
+  char *rollback_instructions = NULL;       // how to use rollback to change default; depending on state
+  char *version_message = NULL;        // is this the most recent version?
   OstreeDeployment *most_recent = NULL; // most recent deployment
   guint i;
   guint j;
@@ -68,61 +68,63 @@ rpmostree_builtin_status (int             argc,
   if (deployments->len < 2)
     {
       g_print ("Note: Fewer than two versions found\n");
-      default_boot_message = default_instruct = version_message = "";
-      goto print;
+      default_boot_message = rollback_instructions = version_message = "";
     }
-    
-  // determine most recent version
-  for (i=0; i<deployments->len; i++)
-  {
-    if (most_recent == NULL)
-      most_recent = deployments->pdata[i];
-    else
+  else
     {
-      gs_unref_variant GVariant *version = NULL;
-      gs_unref_variant GVariant *recent_version = NULL;
-      gs_unref_object OstreeRepo *repo = NULL;
-      const char *csum = ostree_deployment_get_csum(deployments->pdata[i]);
-      const char *recent_csum = ostree_deployment_get_csum(most_recent);
+      // determine most recent version
+      for (i=0; i<deployments->len; i++)
+        {
+          if (most_recent == NULL)
+            most_recent = deployments->pdata[i];
+          else
+            {
+              gs_unref_variant GVariant *version = NULL;
+              gs_unref_variant GVariant *recent_version = NULL;
+              gs_unref_object OstreeRepo *repo = NULL;
+              const char *csum = ostree_deployment_get_csum (deployments->pdata[i]);
+              const char *recent_csum = ostree_deployment_get_csum (most_recent);
 
-      if (!ostree_sysroot_get_repo (sysroot, &repo, cancellable, error))
-        goto out;
+              if (!ostree_sysroot_get_repo (sysroot, &repo, cancellable, error))
+                goto out;
 
-      if (!ostree_repo_load_variant (repo,
-                                   OSTREE_OBJECT_TYPE_COMMIT,
-                                   csum,
-                                   &version,
-                                   error))
-        goto out;
+              if (!ostree_repo_load_variant (repo,
+                                           OSTREE_OBJECT_TYPE_COMMIT,
+                                           csum,
+                                           &version,
+                                           error))
+                goto out;
 
-      if (!ostree_repo_load_variant (repo,
-                                   OSTREE_OBJECT_TYPE_COMMIT,
-                                   recent_csum,
-                                   &recent_version,
-                                   error))
-        goto out;
-      
-      // the newer (most recent) version will have a larger timestamp
-      // i.e. if a > b then a is newer than b
-      if (ostree_commit_get_timestamp (version) > ostree_commit_get_timestamp(recent_version))
-          most_recent = deployments->pdata[i];
+              if (!ostree_repo_load_variant (repo,
+                                           OSTREE_OBJECT_TYPE_COMMIT,
+                                           recent_csum,
+                                           &recent_version,
+                                           error))
+                goto out;
+              
+              // the newer (most recent) version will have a larger timestamp
+              // i.e. if a > b then a is newer than b
+              if (ostree_commit_get_timestamp (version) > ostree_commit_get_timestamp (recent_version))
+                  most_recent = deployments->pdata[i];
+            }
+        }
+
+      if (deployments->pdata[0] == booted_deployment) // if current deployment is default
+        {
+          default_boot_message = "The current booted deployment is the default\n";
+          rollback_instructions = "Use command 'rpm-ostree rollback' to switch defaults\n";
+        }
+      else // current boot not default
+        {
+          default_boot_message = "The current booted deployment is not the default\n";
+          rollback_instructions = "Use command 'rpm-ostree rollback' to make it default\n";
+        }
+      version_message = (booted_deployment == most_recent) 
+                      ? "\nThe current booted deployment is the most recent upgrade\n"
+                      : "\nThe current booted deployment is not the most recent upgrade\n";
     }
-  }
 
-  if (deployments->pdata[0] == booted_deployment) // if current deployment is default
-  {
-    default_boot_message = "";
-    default_instruct = "Use command 'rpm-ostree rollback' to switch defaults\n";
-  }
-  else // current boot not default
-  {
-    default_boot_message = " not";
-    default_instruct = "Use command 'rpm-ostree rollback' to make it default\n";
-  }
-  version_message = (booted_deployment == most_recent) ? "" : " not";
-
-  print:
-    for (j=0; j<deployments->len; j++)
+  for (j=0; j<deployments->len; j++)
     {
       OstreeDeployment *deployment = deployments->pdata[j];
       GKeyFile *origin;
@@ -138,18 +140,18 @@ rpmostree_builtin_status (int             argc,
       if (!origin)
         g_print ("      origin: none\n");
       else
-      {
-        gs_free char *origin_refspec = g_key_file_get_string (origin, "origin", "refspec", NULL);
-        if (!origin_refspec)
-          g_print ("      origin: <unknown origin type>\n");
-        else
-          g_print ("      origin refspec: %s\n", origin_refspec);
-      }
-  }
+        {
+          gs_free char *origin_refspec = g_key_file_get_string (origin, "origin", "refspec", NULL);
+          if (!origin_refspec)
+            g_print ("      origin: <unknown origin type>\n");
+          else
+            g_print ("      origin refspec: %s\n", origin_refspec);
+        }
+    } 
 
-    g_print ("\nThe current booted deployment is%s the most recent upgrade\n", version_message);
-    g_print ("The current booted deployment is%s the default\n", default_boot_message);
-    g_print ("%s", default_instruct);
+    g_print ("%s", version_message);
+    g_print ("%s", default_boot_message);
+    g_print ("%s", rollback_instructions);
 
   ret = TRUE;
   out:
