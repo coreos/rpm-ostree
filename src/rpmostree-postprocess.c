3e84cb249acbaa1b664aeb30e6534a3ebc6bb23e
Colin Walters <walters@verbum.org>
2014-12-04 11:10:17 -0500

    postprocess: Write preserved groups to *both* /etc/group and /usr/lib/group
    Otherwise, upgraded systems which have modified /etc/group (by e.g.
    adding a human user), will actually see the group drop out with bad
    consequences.
    
    It's harmless to have it in both, /etc will override /usr.
    
    Fixes #67

--- src/rpmostree-postprocess.c
+++ src/rpmostree-postprocess.c
@@ -691,7 +691,6 @@ migrate_passwd_file_except_root (GFile         *rootfs,
             break;
         }
 
-      deststream = usrdest_stream;
 
       if (pw)
         {
@@ -704,13 +703,32 @@ migrate_passwd_file_except_root (GFile         *rootfs,
           name = gr->gr_name;
         }
 
-      if (id == 0 || (preserve && g_hash_table_contains (preserve, name)))
+      if (id == 0)
         deststream = etcdest_stream;
+      else
+        deststream = usrdest_stream;
 
       if (pw)
         r = putpwent (pw, deststream);
       else
         r = putgrent (gr, deststream);
+
+      /* If it's marked in the preserve group, we need to write to
+       * *both* /etc and /usr/lib in order to preserve semantics for
+       * upgraded systems from before we supported the preserve
+       * concept.
+       */
+      if (preserve && g_hash_table_contains (preserve, name))
+        {
+          /* We should never be trying to preserve the root entry, it
+           * should always be only in /etc.
+           */
+          g_assert (deststream == usrdest_stream);
+          if (pw)
+            r = putpwent (pw, etcdest_stream);
+          else
+            r = putgrent (gr, etcdest_stream);
+        }
 
       if (r == -1)
         {
