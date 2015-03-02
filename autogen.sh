4118617666c51b447a28901569eb7df9900746f6
Giuseppe Scrivano <gscrivan@redhat.com>
2015-03-02 10:46:08 +0100

    autogen.sh: retrieve libglnx
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- autogen.sh
+++ autogen.sh
@@ -15,6 +15,14 @@ fi
 
 mkdir -p m4
 
+cd $olddir
+if ! test -f libglnx/README.md; then
+    git submodule update --init
+fi
+# Workaround automake bug with subdir-objects and computed paths
+sed -e 's,$(libglnx_srcpath),'${srcdir}/libglnx,g < libglnx/Makefile-libglnx.am >libglnx/Makefile-libglnx.am.inc
+
+
 autoreconf --force --install --verbose
 
 cd $olddir
