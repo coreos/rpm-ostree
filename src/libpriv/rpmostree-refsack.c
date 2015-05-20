cf89acb74c22f7bf3cca9a84ed14270bda3aefd9
Colin Walters <walters@verbum.org>
2015-05-20 13:21:48 -0400

    (cleanup): Move the refsack and root loading code into libpriv
    We had `src/lib` having its own little private library; I wanted to
    use some of it inside `src/libpriv`, so let's consistently have all
    private utility code in `src/libpriv`.
    
    Closes: https://github.com/projectatomic/rpm-ostree/pull/147

--- src/libpriv/rpmostree-refsack.c
+++ src/libpriv/rpmostree-refsack.c
@@ -21,7 +21,8 @@
 #include "config.h"
 
 #include <string.h>
-#include "rpmostree-priv.h"
+#include "rpmostree-refsack.h"
+#include "rpmostree-rpm-util.h"
 
 RpmOstreeRefSack *
 _rpm_ostree_refsack_new (HySack sack, int temp_base_dfd, const char *temp_path)
@@ -57,3 +58,54 @@ _rpm_ostree_refsack_unref (RpmOstreeRefSack *rsack)
   g_free (rsack->temp_path);
   g_free (rsack);
 }
+
+RpmOstreeRefSack *
+_rpm_ostree_get_refsack_for_commit (OstreeRepo                *repo,
+                                    const char                *ref,
+                                    GCancellable              *cancellable,
+                                    GError                   **error)
+{
+  g_autofree char *commit = NULL;
+  g_autofree char *tempdir = g_strdup ("/tmp/rpmostree-dbquery-XXXXXXXX");
+  OstreeRepoCheckoutOptions checkout_options = { 0, };
+  glnx_fd_close int tempdir_dfd = -1;
+
+  if (!ostree_repo_resolve_rev (repo, ref, FALSE, &commit, error))
+    goto out;
+
+  if (mkdtemp (tempdir) == NULL)
+    {
+      glnx_set_error_from_errno (error);
+      goto out;
+    }
+
+  if (!glnx_opendirat (AT_FDCWD, tempdir, FALSE, &tempdir_dfd, error))
+    goto out;
+
+  /* Create intermediate dirs */ 
+  if (!glnx_shutil_mkdir_p_at (tempdir_dfd, "usr/share", 0777, cancellable, error))
+    goto out;
+
+  checkout_options.mode = OSTREE_REPO_CHECKOUT_MODE_USER;
+  checkout_options.subpath = "usr/share/rpm";
+
+  if (!ostree_repo_checkout_tree_at (repo, &checkout_options,
+                                     tempdir_dfd, "usr/share/rpm",
+                                     commit, 
+                                     cancellable, error))
+    goto out;
+
+  {
+    HySack hsack; 
+
+    if (!rpmostree_get_sack_for_root (tempdir_dfd, ".",
+                                      &hsack, cancellable, error))
+      goto out;
+
+    return _rpm_ostree_refsack_new (hsack, AT_FDCWD, tempdir);
+  }
+
+ out:
+  return NULL;
+}
+
