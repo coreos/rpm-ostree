280ec7c9e9506bacae2e167ed6a3b1ba98f4829f
Jonathan Lebon <jlebon@redhat.com>
2016-05-24 17:40:10 -0400

    test-basic.sh: make subtests more granular

--- tests/test-basic.sh
+++ tests/test-basic.sh
@@ -23,7 +23,7 @@ set -e
 
 check_root_test
 
-echo "1..1"
+echo "1..9"
 
 setup_os_repository "archive-z2" "syslinux"
 
@@ -41,6 +41,7 @@ ostree admin --sysroot=sysroot deploy --karg=root=LABEL=MOO --karg=quiet --os=te
 rpm-ostree status | tee OUTPUT-status.txt
 
 assert_file_has_content OUTPUT-status.txt '1\.0\.10'
+echo "ok status shows right version"
 
 os_repository_new_commit
 rpm-ostree upgrade --os=testos
@@ -53,6 +54,7 @@ rpm-ostree status | tee OUTPUT-status.txt
 assert_not_file_has_content OUTPUT-status.txt '1\.0\.10'
 version=$(date "+%Y%m%d\.0")
 assert_file_has_content OUTPUT-status.txt $version
+echo "ok rebase onto newer version"
 
 # Test 'upgrade --check' w/ no upgrade
 # XXX Disabled this because source repo has no /usr/share/rpm
@@ -65,6 +67,7 @@ assert_file_has_content OUTPUT-status.txt $version
 rpm-ostree deploy --os=testos 1.0.9
 rpm-ostree status | head --lines 3 | tee OUTPUT-status.txt
 assert_file_has_content OUTPUT-status.txt '1\.0\.9'
+echo "ok deploy older known version"
 
 # Remember the current revision for later.
 revision=$(ostree rev-parse --repo=sysroot/ostree/repo otheros:testos/buildmaster/x86_64-runtime)
@@ -73,6 +76,7 @@ revision=$(ostree rev-parse --repo=sysroot/ostree/repo otheros:testos/buildmaste
 rpm-ostree deploy --os=testos 1.0.10
 rpm-ostree status | head --lines 3 | tee OUTPUT-status.txt
 assert_file_has_content OUTPUT-status.txt '1\.0\.10'
+echo "ok deploy newer known version"
 
 # Jump forward to a new, locally unknown version.
 # Here we also test the "version=" argument prefix.
@@ -80,12 +84,14 @@ os_repository_new_commit 1 1
 rpm-ostree deploy --os=testos version=$(date "+%Y%m%d.1")
 rpm-ostree status | head --lines 3 | tee OUTPUT-status.txt
 assert_file_has_content OUTPUT-status.txt $(date "+%Y%m%d\.1")
+echo "ok deploy newer unknown version"
 
 # Jump backward again to 1.0.9, but this time using the
 # "revision=" argument prefix (and test case sensitivity).
 rpm-ostree deploy --os=testos REVISION=$revision
 rpm-ostree status | head --lines 3 | tee OUTPUT-status.txt
 assert_file_has_content OUTPUT-status.txt '1\.0\.9'
+echo "ok deploy older version by revision"
 
 # Ensure it returns an error when passing a wrong option.
 rpm-ostree --help | awk '/^$/ {in_commands=0} {if(in_commands==1){print $0}} /^Builtin Commands:/ {in_commands=1}' > commands
@@ -94,8 +100,10 @@ while read command; do
         assert_not_reached "command $command --n0t-3xisting-0ption was successful"
     fi
 done < commands
+echo "ok error on unknown command options"
 
 if rpm-ostree nosuchcommand --nosuchoption 2>err.txt; then
     assert_not_reached "Expected an error for nosuchcommand"
 fi
 assert_file_has_content err.txt 'Unknown.*command'
+echo "ok error on unknown command"
