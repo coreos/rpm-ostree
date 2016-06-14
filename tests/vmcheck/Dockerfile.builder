be3a677125fea3c90b8e6f189aff120ec4dc7358
Jonathan Lebon <jlebon@redhat.com>
2016-06-14 16:27:14 -0400

    vmcheck: create a new deployment instead
    In order for vmcheck to be useful for testing, we need the machine to be
    in a "clean" state. That is, sitting on a commit, ready for being
    manipulated.
    
    This is a small step towards this goal. Instead of overwriting the
    files, we use rofiles-fuse to safely install and create a new
    deployment.
    
    Closes: #321
    Approved by: cgwalters

--- tests/vmcheck/Dockerfile.builder
+++ tests/vmcheck/Dockerfile.builder
@@ -7,7 +7,6 @@ RUN yum -y install yum-plugin-priorities sudo && \
     && yum-builddep -y rpm-ostree
 RUN groupadd -g 1000 vagrant && useradd -u 1000 -g vagrant -G wheel vagrant
 RUN echo "%wheel	ALL=(ALL)	NOPASSWD: ALL" >> /etc/sudoers
-ADD host-install.sh /usr/local/bin/host-install
 LABEL RUN "/usr/bin/docker run --privileged -ti -v /var/home:/home -v /etc:/host/etc -v /usr:/host/usr \${IMAGE}"
 USER vagrant
 WORKDIR /home/vagrant/sync
---
+++ tests/vmcheck/checkout.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+# ugly but simple way of fetching commit we're sitting on
+commit=$(rpm-ostree status --json | \
+  python -c 'import sys, json; print json.load(sys.stdin)[0]["checksum"]')
+
+if [[ -z $commit ]] || ! ostree rev-parse $commit; then
+  echo "Error while determining current commit" >&2
+  exit 1
+fi
+
+cd /ostree/repo/tmp
+umount vmcheck.ro 2>/dev/null ||:
+rm -rf vmcheck*
+ostree checkout $commit vmcheck --fsync=0
+mkdir vmcheck.ro
+rofiles-fuse vmcheck{,.ro}
