103fa8d72017a47a1f8d4cfbbd96b2e96a887e7b
Amila Sampath <pgsamila@yahoo.com>
2016-06-18 23:35:33 +0530

    Auto upgrading with a script
    By running this script, the atomic image can update via rpm-ostree automatically

---
+++ autoupdate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NUM=1
+
+while [ $NUM -eq 1 ]; do
+	
+	ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && conn || nonn
+	
+	function conn {
+    	sudo rpm-ostree upgrade --check
+    	NUM=2
+	}
+
+	function nonn {
+		sleep 60
+	}
+
+done
