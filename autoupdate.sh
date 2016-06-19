98083d21a831bf188d34779831d4032cb7e034b0
Amila Sampath <pgsamila@yahoo.com>
2016-06-19 10:37:50 +0530

    Auto upgrading with a script
    I have change the script because the earlier script did not identify the functions which I made. This script work fine.

--- autoupdate.sh
+++ autoupdate.sh
@@ -2,17 +2,17 @@
 
 NUM=1
 
+function conn {
+	sudo rpm-ostree upgrade --check
+	NUM=2
+}
+
+function nonn {
+	sleep 60
+}
+	
 while [ $NUM -eq 1 ]; do
 	
 	ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && conn || nonn
 	
-	function conn {
-    	sudo rpm-ostree upgrade --check
-    	NUM=2
-	}
-
-	function nonn {
-		sleep 60
-	}
-
 done
