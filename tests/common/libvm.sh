812c4318569d1f4599a696b6ea20aa7d5e706b3d
Jonathan Lebon <jlebon@redhat.com>
2016-06-21 17:35:51 -0400

    test-layering.sh: basic package layering test
    This is a preliminary package layering test which simply installs the
    foo package and verifies that it functions properly. A bunch of
    primitives are added to libvm.sh to facilitate this and future tests.
    
    Closes: #344
    Approved by: cgwalters

--- tests/common/libvm.sh
+++ tests/common/libvm.sh
@@ -17,6 +17,74 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
+# run command in vm
+# - $@    command to run
 vm_cmd() {
   $SSH "$@"
+}
+
+# copy files to a directory in the vm
+# - $1    target directory
+# - $2..  files & dirs to copy
+vm_send() {
+  dir=$1; shift
+  vm_cmd mkdir -p $dir
+  $SCP -r "$@" vmcheck:$dir
+}
+
+# wait until ssh is available on the vm
+vm_ssh_wait() {
+  # XXX: add timeout
+  while ! vm_cmd true &> /dev/null; do
+    sleep 1
+  done
+}
+
+# reboot the vm
+vm_reboot() {
+  vm_cmd systemctl reboot || :
+  sleep 2 # give time for port to go down
+  vm_ssh_wait
+}
+
+# check that the given files exist on the VM
+# - $@    packages to check for
+vm_has_files() {
+  for file in "$@"; do
+    if ! vm_cmd test -e $file; then
+        return 1
+    fi
+  done
+}
+
+# check that the packages are installed
+# - $@    packages to check for
+vm_has_packages() {
+  for pkg in "$@"; do
+    if ! vm_cmd rpm -q $pkg; then
+        return 1
+    fi
+  done
+}
+
+# print the layered packages
+vm_get_layered_packages() {
+  vm_cmd rpm-ostree status --json | \
+    python -c '
+import sys, json
+depl = json.load(sys.stdin)["deployments"][0]
+if "packages" in depl:
+  print " ".join(depl["packages"])
+'
+}
+
+# check that the packages are currently layered
+# - $@    packages to check for
+vm_has_layered_packages() {
+  pkgs=$(vm_get_layered_packages)
+  for pkg in "$@"; do
+    if [[ " $pkgs " != *$pkg* ]]; then
+        return 1
+    fi
+  done
 }
