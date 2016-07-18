coreos/rpm-ostree

commit 749a5f03a8586392cafff949075c45a040d4e984

daemon: don't try to resolve rev
During the initialization of the daemon, it would try to load the
deployments and cache a few things. One of these steps wanted to look up
the rev of the deployment. However, if the rev no longer exists (e.g. we
just did a rebase, purged the ref, and rebooted), this would error out.

Replace the lookup by simply directly using the csum from the
deployment.

src/daemon/rpmostreed-deployment-utils.c
@@ -259,7 +259,7 @@ rpmostreed_commit_generate_cached_details_variant (OstreeDeployment *deployment,
 {
   g_autoptr(GVariant) commit = NULL;
   g_autofree gchar *origin_refspec = NULL;
-  g_autofree gchar *head = NULL;
+  const gchar *head = NULL;
   const gchar *osname;
   GVariant *sigs = NULL; /* floating variant */
   GVariant *ret = NULL; /* floating variant */
@@ -275,9 +275,8 @@ rpmostreed_commit_generate_cached_details_variant (OstreeDeployment *deployment,
   if (!origin_refspec)
     goto out;
 
-  if (!ostree_repo_resolve_rev (repo, origin_refspec,
-                                FALSE, &head, error))
-    goto out;
+  head = ostree_deployment_get_csum (deployment);
+
   if (!ostree_repo_load_variant (repo,
 				 OSTREE_OBJECT_TYPE_COMMIT,
 				 head,

commit 0d03b1cf444f876d7ee0ef79e0f814a92964b00d

daemon: Do resolve rev for deployment variant details
Previously in #353 we changed this to not resolve the rev, but
that breaks the `CachedUpdateDetails` which actually wants it
to be resolved, to show pending updates.

This effectively reverts 749a5f03a8586392cafff949075c45a040d4e984
and changes things so we set `allow_noent=TRUE` and if there is
no ref, just return the deployment checksum.

Fixes: 749a5f03a8586392cafff949075c45a040d4e984
Closes: #392

src/daemon/rpmostreed-deployment-utils.c
@@ -259,7 +259,7 @@ rpmostreed_commit_generate_cached_details_variant (OstreeDeployment *deployment,
 {
   g_autoptr(GVariant) commit = NULL;
   g_autofree gchar *origin_refspec = NULL;
-  const gchar *head = NULL;
+  g_autofree gchar *head = NULL;
   const gchar *osname;
   GVariant *sigs = NULL; /* floating variant */
   GVariantDict dict;
@@ -273,7 +273,15 @@ rpmostreed_commit_generate_cached_details_variant (OstreeDeployment *deployment,
 
   g_assert (origin_refspec);
 
-  head = ostree_deployment_get_csum (deployment);
+  /* allow_noent=TRUE since the ref may have been deleted for a
+   * rebase.
+   */
+  if (!ostree_repo_resolve_rev (repo, origin_refspec,
+                                TRUE, &head, error))
+    return NULL;
+
+  if (head == NULL)
+    head = g_strdup (ostree_deployment_get_csum (deployment));
 
   if (!ostree_repo_load_variant (repo,
 				 OSTREE_OBJECT_TYPE_COMMIT,