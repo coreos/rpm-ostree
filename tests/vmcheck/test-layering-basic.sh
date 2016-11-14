17f48b4b27cad0df0dfaba8e9790f89df17a1723
Colin Walters <walters@verbum.org>
2016-11-14 14:28:38 -0500

    tests: Test pinned package version
    It should work for both layering and treecompose.

--- tests/vmcheck/test-layering-basic.sh
+++ tests/vmcheck/test-layering-basic.sh
@@ -34,7 +34,7 @@ vm_send_test_repo
 # make sure the package is not already layered
 vm_assert_layered_pkg foo absent
 
-vm_cmd rpm-ostree pkg-add foo
+vm_cmd rpm-ostree pkg-add foo-1.0
 echo "ok pkg-add foo"
 
 vm_reboot
