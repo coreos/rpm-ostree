b7dfadc6c81b4014d1c3d988825cf12cae51dd09
Jonathan Lebon <jlebon@redhat.com>
2016-12-09 11:16:52 -0500

    fixup! compose-tests: add more tests

--- tests/compose-tests/test-install-langs.sh
+++ tests/compose-tests/test-install-langs.sh
@@ -12,6 +12,8 @@ cat > lang-test.sh << EOF
 set -xeuo pipefail
 env LANG=fr_FR.UTF-8 date -ud @0 &> /etc/lang-test.date.txt
 (env LANG=fr_FR.UTF-8 touch || :) &> /etc/lang-test.touch.txt
+env LANG=de_DE.UTF-8 date -ud @0 &> /etc/lang-test.de.date.txt
+(env LANG=de_DE.UTF-8 touch || :) &> /etc/lang-test.de.touch.txt
 EOF
 chmod a+x lang-test.sh
 runcompose
@@ -21,4 +23,11 @@ ostree --repo=${repobuild} cat ${treeref} /usr/etc/lang-test.date.txt > out.txt
 assert_file_has_content out.txt 'jeu\. janv\.  1 00:00:00 UTC 1970'
 ostree --repo=${repobuild} cat ${treeref} /usr/etc/lang-test.touch.txt > out.txt
 assert_file_has_content out.txt 'opérande de fichier manquant'
+
+# check that de_DE was culled
+ostree --repo=${repobuild} cat ${treeref} /usr/etc/lang-test.de.date.txt > out.txt
+assert_file_has_content out.txt 'Thu Jan  1 00:00:00 UTC 1970'
+ostree --repo=${repobuild} cat ${treeref} /usr/etc/lang-test.de.touch.txt > out.txt
+assert_file_has_content out.txt 'missing file operand'
+
 echo "ok install-langs"
