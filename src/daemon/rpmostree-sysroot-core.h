eaa0b20dc8faaa831dec9d8c25d062cc052ef1ed
Colin Walters <walters@verbum.org>
2017-03-27 14:12:05 -0400

    daemon: Introduce a "sysroot core" file
    Right now, the `rpm-ostree cleanup` logic calls into a "public static" method of
    `RpmOstreeSysrootUpgrader`. That's because today, a lot of the high level "core
    server side" logic lived in that class. However, it's only for upgrades (well,
    really "generating pending deployments").
    
    As I'm working on "livefs" which is really something different, I find it needs
    to interact with some of `SysrootUpgrader`'s logic. Let's introduce a "sysroot
    core" which is just a set of static functions taking an `OstreeSysroot*`, and
    then all 3 of cleanup, (upgrade|deploy) and livefs can use these.

---
+++ src/daemon/rpmostree-sysroot-core.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2017 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#pragma once
+
+#include <ostree.h>
+
+#include "rpmostreed-types.h"
+
+/* Used by the upgrader to hold a strong ref temporarily to a base commit */
+#define RPMOSTREE_TMP_BASE_REF "rpmostree/base/tmp"
+/* Where we check out a new rootfs */
+#define RPMOSTREE_TMP_ROOTFS_DIR "extensions/rpmostree/commit"
+
+gboolean
+rpmostree_syscore_cleanup (OstreeSysroot            *sysroot,
+                           OstreeRepo               *repo,
+                           GCancellable             *cancellable,
+                           GError                  **error);
