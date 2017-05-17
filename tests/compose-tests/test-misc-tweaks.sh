69d8bfa0420d1399b9a2491786ae5a72a54b5100
Colin Walters <walters@verbum.org>
2017-05-17 14:52:24 -0400

    compose-tests: Consolidate some misc options into `test-misc-tweaks.sh`
    I was about to add another one of these but it feels like a bit
    overkill to run through a recompose for trivial tweaks like turning off docs.
    
    We can do a compose with multiple options at once and test the result as a unit,
    at least for the smaller/less invasive options.
    
    This change is prep for adding a switch to do `/tmp` as a regular dir.
    
    Closes: #777
    Approved by: jlebon

---
+++ tests/compose-tests/test-misc-tweaks.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+dn=$(cd $(dirname $0) && pwd)
+. ${dn}/libcomposetest.sh
+
+prepare_compose_test "misc-tweaks"
+# No docs
+pysetjsonmember "documentation" "False"
+# And tweak some of the systemd units
+pysetjsonmember "default_target" '"multi-user.target"'
+pyappendjsonmember "packages" '["tuned"]'
+pysetjsonmember "units" '["tuned.service"]'
+# And test adding/removing files
+pysetjsonmember "add-files" '[["foo.txt", "/usr/etc/foo.txt"]]'
+pysetjsonmember "remove-files" '["etc/hosts"]'
+pysetjsonmember "remove-from-packages" '[["setup", "/etc/hosts\..*"]]'
+rnd=$RANDOM
+echo $rnd > composedata/foo.txt
+
+# Do the compose
+runcompose
+echo "ok compose"
+
+# Tests for nodocs
+ostree --repo=${repobuild} ls -R ${treeref} /usr/share/man > manpages.txt
+assert_not_file_has_content manpages.txt man5/ostree.repo.5
+echo "ok no manpages"
+
+# Tests for units
+ostree --repo=${repobuild} ls ${treeref} \
+       /usr/etc/systemd/system/default.target > out.txt
+assert_file_has_content out.txt '-> .*/multi-user\.target'
+echo "ok default target"
+
+ostree --repo=${repobuild} ls ${treeref} \
+       /usr/etc/systemd/system/multi-user.target.wants > out.txt
+assert_file_has_content out.txt '-> .*/tuned.service'
+echo "ok enable units"
+
+# Tests for files
+ostree --repo=${repobuild} cat ${treeref} /usr/etc/foo.txt > out.txt
+assert_file_has_content out.txt $rnd
+echo "ok add-files"
+
+ostree --repo=${repobuild} ls ${treeref} /usr/etc > out.txt
+assert_not_file_has_content out.txt '/usr/etc/hosts$'
+echo "ok remove-files"
+
+ostree --repo=${repobuild} ls ${treeref} /usr/etc > out.txt
+assert_not_file_has_content out.txt '/usr/etc/hosts\.allow$'
+assert_not_file_has_content out.txt '/usr/etc/hosts\.deny$'
+echo "ok remove-from-packages"
