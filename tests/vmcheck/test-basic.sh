ae1f8c78d3289ff6ac78423bcef33c684d093556
Jonathan Lebon <jlebon@redhat.com>
2017-06-05 12:38:39 -0400

    vmcheck: create new test-basic.sh test
    And move tests from test-layering-basic.sh that aren't related to
    pkglayering there.

---
+++ tests/vmcheck/test-basic.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 Red Hat, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+set -e
+
+. ${commondir}/libtest.sh
+. ${commondir}/libvm.sh
+
+set -x
+
+# make sure that package-related entries are always present,
+# even when they're empty
+vm_assert_status_jq \
+  '.deployments[0]["packages"]' \
+  '.deployments[0]["requested-packages"]' \
+  '.deployments[0]["requested-local-packages"]' \
+  '.deployments[0]["removed-base-packages"]'
+echo "ok empty pkg arrays in status json"
+
+# Be sure an unprivileged user exists
+vm_cmd getent passwd bin
+
+# Make sure we can't layer as non-root
+if vm_cmd "runuser -u bin rpm-ostree pkg-add foo-1.0"; then
+    assert_not_reached "Was able to install a package as non-root!"
+fi
+echo "ok layering requires root"
+
+# Assert that we can do status as non-root
+vm_cmd "runuser -u bin rpm-ostree status"
+echo "ok status doesn't require root"
