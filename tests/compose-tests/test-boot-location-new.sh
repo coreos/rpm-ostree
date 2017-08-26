f113fc5e27bad39703eac73b625ae450038e8f85
Colin Walters <walters@verbum.org>
2017-08-25 21:06:09 -0400

    Rework treecompose kernel processing
    Prep for changing `boot_location: new` to use `/usr/lib/ostree-boot`
    and `/usr/lib/modules`.  Rework our kernel postprocessing
    so that we unify the `boot_location` handling with initramfs generation.
    
    Instead of doing the initramfs first in postprocessing, we do it nearly last,
    after e.g. `etc` is renamed to `usr/etc`. This has some consequences, such as
    the fact that `run_bwrap_mutably()` is now called in both situations. In
    general, our handling of `etc` is inconsistent, although understandably so.
    
    As part of this, I finally got around to implementing the bit from
    https://github.com/systemd/systemd/pull/4174 however suboptimal it is; need the
    unified core so we can cleanly ignore the posttrans like we do others.  We
    intentionally keep the file around in the generated tree so that installing a
    kernel RPM per client doesn't try to do any of this either.
    
    This all gets folded together so that the logic for handling the bootloader gets
    simpler - in the Fedora case, we now know to find kernels in `/usr/lib/modules`
    and can ignore `/boot`.
    
    Closes: #959
    Approved by: jlebon

---
+++ tests/compose-tests/test-boot-location-new.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -xeuo pipefail
+
+dn=$(cd $(dirname $0) && pwd)
+. ${dn}/libcomposetest.sh
+
+prepare_compose_test "bootlocation-new"
+pysetjsonmember "boot_location" '"new"'
+runcompose
+echo "ok compose"
+
+# Nothing in /boot (but it should exist)
+ostree --repo=${repobuild} ls -R ${treeref} /boot > bootls.txt
+cat >bootls-expected.txt <<EOF
+d00755 0 0      0 /boot
+EOF
+diff -u bootls{-expected,}.txt
+# Verify /usr/lib/ostree-boot
+ostree --repo=${repobuild} ls -R ${treeref} /usr/lib/ostree-boot > bootls.txt
+assert_file_has_content bootls.txt vmlinuz-
+assert_file_has_content bootls.txt initramfs-
+kver=$(grep /vmlinuz bootls.txt | sed -e 's,.*/vmlinuz-\(.*\)-[0-9a-e].*$,\1,')
+# And use the kver to find the kernel in /usr/lib/modules
+ostree --repo=${repobuild} ls ${treeref} /usr/lib/modules/${kver}/{vmlinuz,initramfs.img} >/dev/null
+echo "ok boot location new"
