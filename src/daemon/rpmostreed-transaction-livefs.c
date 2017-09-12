cfe2901afdebfcb04a44f1746a0445887ad6f8e9
Jonathan Lebon <jlebon@redhat.com>
2017-09-12 21:11:43 +0000

    livefs: tweak warning re. /etc
    Be more explicit about what the warning means.

--- src/daemon/rpmostreed-transaction-livefs.c
+++ src/daemon/rpmostreed-transaction-livefs.c
@@ -659,8 +659,9 @@ livefs_transaction_execute_inner (LiveFsTransaction *self,
         return FALSE;
     }
 
-  /* XXX: right now we don't have a good solution for this */
-  rpmostree_output_message ("WARNING: changes to /etc are not currently preserved!");
+  /* XXX: right now we don't have a good solution for this:
+   * https://github.com/projectatomic/rpm-ostree/issues/40 */
+  rpmostree_output_message ("WARNING: changes to /etc will not appear in the pending deployment!");
 
   /* Write out the origin as having completed this */
   if (!write_livefs_state (sysroot, booted_deployment, NULL, target_csum, error))
