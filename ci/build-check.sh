69ab0e4990df992ff6d1ec6ae38e7f6c9e731648
Jonathan Lebon <jlebon@redhat.com>
2017-09-14 20:03:29 +0000

    fixup! ci: don't run clang on CentOS

--- ci/build-check.sh
+++ ci/build-check.sh
@@ -10,13 +10,13 @@ ${dn}/build.sh
 # support parallel runs right now
 /usr/bin/make check
 make install
-git clean -dfx
 
 # And now a clang build to find unused variables, but only run on Fedora because
 # the CentOS version is ancient anyway and doesn't support all the flags that
 # might get passed to it.
 id=$(. /etc/os-release && echo $ID)
 if [ "$id" == fedora ]; then
+    git clean -dfx
     export CC=clang
     export CFLAGS='-Werror=unused-variable -Werror=maybe-uninitialized'
     build_default
