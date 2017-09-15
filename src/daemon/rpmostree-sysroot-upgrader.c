7e7668bc1111b96418d64252f6e3904baf91c8f7
Colin Walters <walters@verbum.org>
2017-09-15 09:52:24 -0400

    fixup! daemon/upgrader: Clear core ctx after committing

--- src/daemon/rpmostree-sysroot-upgrader.c
+++ src/daemon/rpmostree-sysroot-upgrader.c
@@ -962,8 +962,11 @@ perform_local_assembly (RpmOstreeSysrootUpgrader *self,
                                            cancellable, error))
     return FALSE;
 
-  /* Ensure we aren't holding any references to the tmpdir now that we're
-   * done. */
+  /* Ensure we aren't holding any references to the tmpdir now that we're done;
+   * rpmostree_sysroot_upgrader_deploy() eventually calls
+   * rpmostree_syscore_cleanup() which deletes 🗑 the tmpdir.  See also similar
+   * bits in the compose and container path.
+   */
   g_clear_object (&self->ctx);
   (void) close (self->tmprootfs_dfd);
   self->tmprootfs_dfd = -1;
