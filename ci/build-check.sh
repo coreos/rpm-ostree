5db2389fbb68aa185f881446fa2d66520a14a2e4
Jonathan Lebon <jlebon@redhat.com>
2017-10-04 19:48:26 +0000

    ci: run clang build with -Werror
    Start running `clang` with `-Werror` like ostree. We can only run this
    on Fedora right now because the CentOS `clang` doesn't support
    `-Wno-error=macro-redefined`, though that's fine.
    
    Closes: #1036
    Approved by: cgwalters

--- ci/build-check.sh
+++ ci/build-check.sh
@@ -10,3 +10,17 @@ ${dn}/build.sh
 # support parallel runs right now
 /usr/bin/make check
 make install
+
+# And now a clang build with -Werror turned on. We can't do this with gcc (in
+# build.sh) because it doesn't support -Wno-error=macro-redefined, (and neither
+# does clang on CentOS). Anyway, all we want is at least one clang run.
+if test -x /usr/bin/clang; then
+  # Except unused-command-line-argument:
+  #   error: argument unused during compilation: '-specs=/usr/lib/rpm/redhat/redhat-hardened    -cc1' [-Werror,-Wunused-command-line-argument]
+  # Except for macro-redefined:
+  #   /usr/include/python2.7/pyconfig-64.h:1199:9: error: '_POSIX_C_SOURCE' macro redefined
+  export CFLAGS="-Wall -Werror -Wno-error=macro-redefined -Wno-error=unused-command-line-argument ${CFLAGS:-}"
+  export CC=clang
+  git clean -dfx && git submodule foreach git clean -dfx
+  build
+fi
