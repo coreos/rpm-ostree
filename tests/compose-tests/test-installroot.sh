18ca99ffc20773c220f5db9010c10f7351781aef
Colin Walters <walters@verbum.org>
2017-10-25 12:38:09 -0400

    fixup! bin/compose: Expose phases as [install, postprocess, commit] cmds

--- tests/compose-tests/test-installroot.sh
+++ tests/compose-tests/test-installroot.sh
@@ -6,7 +6,8 @@ dn=$(cd $(dirname $0) && pwd)
 . ${dn}/libcomposetest.sh
 
 prepare_compose_test "installroot"
-compose_base_argv="--repo=${repobuild} --cache-only --cachedir=${test_compose_datadir}/cache"
+# This is used to test postprocessing with treefile vs not
+pysetjsonmember "boot_location" '"new"'
 instroot_tmp=$(mktemp -d /var/tmp/rpm-ostree-instroot.XXXXXX)
 rpm-ostree compose install ${compose_base_argv} ${treefile} ${instroot_tmp}
 instroot=${instroot_tmp}/rootfs
@@ -24,11 +25,22 @@ cp -al ${instroot}{-postprocess,-postprocess-treefile}
 integrationconf=usr/lib/tmpfiles.d/rpm-ostree-0-integration.conf
 
 assert_not_has_file ${instroot}-postprocess/${integrationconf}
-rpm-ostree compose postprocess  ${instroot}-postprocess
-rpm-ostree compose postprocess ${instroot}-postprocess-treefile
+rpm-ostree compose postprocess ${instroot}-postprocess
 assert_has_file ${instroot}-postprocess/${integrationconf}
+# Without treefile, kernels end up in "both" mode
+ls ${instroot}-postprocess/boot > ls.txt
+assert_file_has_content ls.txt '^vmlinuz-'
+rm -f ls.txt
 ostree --repo=${repobuild} commit -b test-directcommit --selinux-policy ${instroot}-postprocess --tree=dir=${instroot}-postprocess
 echo "ok postprocess + direct commit"
+
+rpm-ostree compose postprocess ${instroot}-postprocess-treefile ${treefile}
+assert_has_file ${instroot}-postprocess-treefile/${integrationconf}
+# with treefile, no kernels in /boot
+ls ${instroot}-postprocess-treefile/boot > ls.txt
+assert_not_file_has_content ls.txt '^vmlinuz-'
+rm -f ls.txt
+echo "ok postprocess with treefile"
 
 testdate=$(date)
 echo "${testdate}" > ${instroot}-directcommit/usr/share/rpm-ostree-composetest-split.txt
