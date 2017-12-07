a7062e64c2623f27ab401afddabc02e9a8ca0820
Colin Walters <walters@verbum.org>
2017-12-07 13:34:39 -0500

    fixup! Add `cancel` verb and DBus API

--- src/app/rpmostree-builtin-cancel.c
+++ src/app/rpmostree-builtin-cancel.c
@@ -65,7 +65,6 @@ rpmostree_builtin_cancel (int             argc,
 {
   g_autoptr(GOptionContext) context = g_option_context_new ("");
   glnx_unref_object RPMOSTreeSysroot *sysroot_proxy = NULL;
-  g_autofree char *transaction_address = NULL;
   _cleanup_peer_ GPid peer_pid = 0;
 
   if (!rpmostree_option_context_parse (context,
