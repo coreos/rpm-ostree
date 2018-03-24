779f764fcd5248e5b70ef4605e77e47f0f36e522
Colin Walters <walters@verbum.org>
2018-03-24 07:52:19 -0400

    app: Call Reload right after getting sysroot proxy
    A final followup to our pile of "add reload" PRs.  This one is
    necessary to fix the `status` race.
    
    Closes: #1312
    Approved by: jlebon

--- tests/vmcheck/test-basic.sh
+++ tests/vmcheck/test-basic.sh
@@ -116,11 +116,9 @@ echo "ok status -b"
 
 # Pinning
 vm_cmd ostree admin pin 0
-vm_rpmostree reload  # Try to avoid reload races
 vm_rpmostree status > status.txt
 assert_file_has_content_literal status.txt "Pinned: yes"
 vm_cmd ostree admin pin -u 0
-vm_rpmostree reload  # Try to avoid reload races
 vm_rpmostree status > status.txt
 assert_not_file_has_content status.txt "Pinned: yes"
 echo "ok pinning"
