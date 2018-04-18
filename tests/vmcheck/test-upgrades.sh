ebc08e9cd9a799c81d0bd480e57b4ced88dd9ebc
Colin Walters <walters@verbum.org>
2018-04-18 14:02:02 -0400

    tests: Move upgrade/rebase tests from unit to vmcheck/test-upgrades
    Now we stop running rpm-ostreed as non-root, which is going to be
    a maintenance pain going forward.  If we do introduce non-VM based
    tests I think we should look to doing in-container testing.

---
+++ tests/vmcheck/test-upgrades.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# Copyright (C) 2018 Red Hat, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+set -euo pipefail
+
+. ${commondir}/libtest.sh
+. ${commondir}/libvm.sh
+
+set -x
+
+# Prepare an OSTree repo with updates
+vm_ostreeupdate_prepare
+
+vm_rpmostree rebase vmcheckmote:vmcheck
+vm_reboot
+vm_rpmostree cleanup -pr
+vm_assert_status_jq ".deployments[0][\"origin\"] == \"vmcheckmote:vmcheck\"" \
+                    ".deployments[0][\"booted\"]" \
+                    ".deployments[0][\"version\"] == \"v1\""
+vm_rpmostree status > status.txt
+assert_file_has_content_literal status.txt 'auto updates disabled'
+# start it up again since we rebooted
+vm_start_httpd ostree_server $REMOTE_OSTREE 8888
+echo "ok prep"
+
+# Test base ostree update
+vm_ostreeupdate_create v2
+vm_rpmostree upgrade
+vm_reboot
+vm_start_httpd ostree_server $REMOTE_OSTREE 8888
+vm_rpmostree cleanup -pr
+vm_assert_status_jq ".deployments[0][\"booted\"]" \
+                    ".deployments[0][\"version\"] == \"v2\""
+echo "ok upgrade"
+
+vm_cmd ostree remote add otherremote --no-gpg-verify http://localhost:8888/
+vm_rpmostree reload
+vm_rpmostree rebase otherremote:
+vm_assert_status_jq ".deployments[0][\"origin\"] == \"otherremote:vmcheck\"" \
+                    ".deployments[0][\"booted\"]|not"
+vm_rpmostree rebase vmcheckmote:
+vm_assert_status_jq ".deployments[0][\"origin\"] == \"vmcheckmote:vmcheck\"" \
+                    ".deployments[0][\"booted\"]|not"
+echo "ok rebase"
+
+# A new update is available...but deploy v1 again
+vm_ostreeupdate_create v3
+vm_rpmostree deploy v1
+vm_assert_status_jq ".deployments[0][\"booted\"]|not" \
+                    ".deployments[0][\"version\"] == \"v1\""
+vm_rpmostree cleanup -pr
+vm_rpmostree deploy version=v3
+vm_assert_status_jq ".deployments[0][\"booted\"]|not" \
+                    ".deployments[0][\"version\"] == \"v3\""
+v3rev=$(vm_get_deployment_info 0 checksum)
+vm_rpmostree deploy v1
+vm_rpmostree cleanup -pr
+vm_rpmostree deploy REVISION=${v3rev}
+vm_assert_status_jq ".deployments[0][\"booted\"]|not" \
+                    ".deployments[0][\"version\"] == \"v3\""
+vm_rpmostree deploy v1
+vm_reboot
+vm_start_httpd ostree_server $REMOTE_OSTREE 8888
+vm_rpmostree cleanup -pr
+vm_rpmostree rebase otherremote:vmcheck ${v3rev}
+vm_assert_status_jq ".deployments[0][\"booted\"]|not" \
+                    ".deployments[0][\"version\"] == \"v3\""
+
+echo "ok deploy"
