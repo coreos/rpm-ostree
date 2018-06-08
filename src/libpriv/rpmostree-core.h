df4e1d7b75ef79f74e0533c564661519c57ac000
Jonathan Lebon <jonathan@jlebon.com>
2018-06-08 15:20:10 -0400

    libpriv: Move cache-branch functions to rpm-util.c
    Prep for later commit.
    
    I hesitated between `rpmostree-util.c` and `rpmostree-rpm-util.c`. In
    the end, I decided on the latter since some of the functions require
    rpmlib headers, which aren't currently included in `rpmostree-util.c`.
    
    Closes: #1395
    Approved by: cgwalters

--- src/libpriv/rpmostree-core.h
+++ src/libpriv/rpmostree-core.h
@@ -123,12 +123,6 @@ gboolean rpmostree_context_get_state_sha512 (RpmOstreeContext *self,
                                              char            **out_checksum,
                                              GError          **error);
 
-char * rpmostree_get_cache_branch_for_n_evr_a (const char *name, const char *evr, const char *arch);
-char *rpmostree_get_cache_branch_header (Header hdr);
-char *rpmostree_get_rojig_branch_header (Header hdr);
-char *rpmostree_get_cache_branch_pkg (DnfPackage *pkg);
-char *rpmostree_get_rojig_branch_pkg (DnfPackage *pkg);
-
 gboolean
 rpmostree_find_cache_branch_by_nevra (OstreeRepo    *pkgcache,
                                       const char    *nevra,
