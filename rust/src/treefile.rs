e731f59007f62fbaed26931ae83bb9c5eebb12ef
Colin Walters <walters@verbum.org>
2018-07-04 19:17:51 +0000

    rust: Split out treefile into its own module
    Further prep for adding more code here.  The `lib.rs` then is the
    collection of glue functions; perhaps in some ideal future it could
    be generated even.
    
    Closes: #1444
    Approved by: jlebon

--- rust/src/treefile.rs
+++ rust/src/treefile.rs
@@ -20,44 +20,13 @@
  * https://github.com/cgwalters/coreos-assembler
  * */
 
-extern crate gio_sys;
-extern crate glib;
-extern crate glib_sys;
-extern crate libc;
+use serde_json;
+use serde_yaml;
 
-#[macro_use]
-extern crate serde_derive;
-extern crate serde;
-extern crate serde_json;
-extern crate serde_yaml;
-
-use std::ffi::{CStr, OsStr};
-use std::os::unix::ffi::OsStrExt;
-use std::os::unix::io::FromRawFd;
 use std::path::Path;
 use std::{fs, io};
 
-mod glibutils;
-use glibutils::*;
-
-#[no_mangle]
-pub extern "C" fn rpmostree_rs_treefile_read(
-    filename: *const libc::c_char,
-    fd: libc::c_int,
-    error: *mut *mut glib_sys::GError,
-) -> libc::c_int {
-    // using an O_TMPFILE is an easy way to avoid ownership transfer issues w/ returning allocated
-    // memory across the Rust/C boundary
-
-    // let's just get the unsafory stuff (see what I did there?) out of the way first
-    let output = io::BufWriter::new(unsafe { fs::File::from_raw_fd(libc::dup(fd)) });
-    let c_str: &CStr = unsafe { CStr::from_ptr(filename) };
-    let filename_path = Path::new(OsStr::from_bytes(c_str.to_bytes()));
-
-    treefile_read_impl(filename_path, output).to_glib_convention(error)
-}
-
-fn treefile_read_impl<W: io::Write>(filename: &Path, output: W) -> io::Result<()> {
+pub fn treefile_read_impl<W: io::Write>(filename: &Path, output: W) -> io::Result<()> {
     let f = io::BufReader::new(fs::File::open(filename)?);
 
     let mut treefile: TreeComposeConfig = match serde_yaml::from_reader(f) {
