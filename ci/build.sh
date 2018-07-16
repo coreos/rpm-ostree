0cc002fd8dbbf4995b27fd52786b3a41fe9cf61b
Colin Walters <walters@verbum.org>
2018-07-16 15:05:37 -0400

    build-sys: Use python3 for libdnf by default if available
    Probably at some point libdnf will drop py2 support, but the
    main reason I'm doing this is avoids a python2 dependency
    for rpm-ostree for distributions that don't want that.
    
    Note of course rpm-ostree itself doesn't use python, libdnf does.
    And only for the python bindings, which we don't use either.  So
    this is mostly just to DTRT automatically for the libdnf bits; down
    the line we could probably add a patch to make the python fully
    conditional.
    
    Closes: #1460
    Approved by: jlebon

--- ci/build.sh
+++ ci/build.sh
@@ -23,6 +23,8 @@ fi
 
 pkg_upgrade
 pkg_install_builddeps rpm-ostree
+# Temporary until spec file changes are upstreamed
+pkg_install_if_os fedora python3-sphinx python3-devel
 # Mostly dependencies for tests
 pkg_install ostree{,-devel,-grub2} createrepo_c /usr/bin/jq PyYAML \
     libubsan libasan libtsan elfutils fuse sudo python-gobject-base \
