11224713d04eba3a56c7e56e76c52d55cb6dd18c
Colin Walters <walters@verbum.org>
2018-09-05 17:38:02 -0400

    fixup! Use cbindgen

---
+++ rust/build.rs
@@ -0,0 +1,24 @@
+// See https://bugzilla.redhat.com/show_bug.cgi?id=1608670#c3
+// and https://github.com/projectatomic/rpm-ostree/pull/1516
+
+extern crate cbindgen;
+
+fn run() -> Result<(), String> {
+    let out_dir_v = std::env::var("OUT_DIR").expect("OUT_DIR is unset");
+    let out_dir = std::path::Path::new(&out_dir_v);
+    let bindings = cbindgen::generate(std::path::Path::new(".")).map_err(|e| e.to_string())?;
+    // This uses unwraps internally; it'd be good to submit a patch
+    // to add a Result-based API.
+    bindings.write_to_file(out_dir.join("rpmostree-rust.h"));
+    Ok(())
+}
+
+fn main() {
+    match run() {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("error: {}", e);
+            std::process::exit(1)
+        }
+    }
+}
