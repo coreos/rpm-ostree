5a673370c2c4938273095896703c024e1fd35662
Robert Fairley <rfairley@redhat.com>
2018-09-12 14:13:29 -0400

    initramfs: Return error when --arg specified without --enable
    This change gives an error when --arg is specified on its own.
    
    Previously, if adding --arg without --enable, no changes occur and
    only the initramfs regeneration status is printed. Since --arg only
    takes effect when used with --enable, this should also give an error
    (or let the user know in some way).

--- src/app/rpmostree-builtin-initramfs.c
+++ src/app/rpmostree-builtin-initramfs.c
@@ -95,6 +95,12 @@ rpmostree_builtin_initramfs (int             argc,
                                "--reboot must be used with --enable or --disable");
           return FALSE;
         }
+      if (opt_add_arg)
+        {
+          g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                               "--arg must be used with --enable");
+          return FALSE;
+        }
 
       if (g_variant_n_children (deployments) > 1)
         {
