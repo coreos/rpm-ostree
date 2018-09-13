3854a8878c46f1304fd017ce5ac54be16fd3b67a
Colin Walters <walters@verbum.org>
2018-09-13 20:10:50 +0000

    build: Regenerate rpmostree-rust.h if cbindgen.toml changes
    So when I edit it I get the changes.  Figuring this out
    took me a little while, we were missing two dependencies.

--- rust/build.rs
+++ rust/build.rs
@@ -6,6 +6,10 @@ extern crate cbindgen;
 fn run() -> Result<(), String> {
     let out_dir_v = std::env::var("OUT_DIR").expect("OUT_DIR is unset");
     let out_dir = std::path::Path::new(&out_dir_v);
+
+    // First, output our dependencies https://doc.rust-lang.org/cargo/reference/build-scripts.html
+    println!("cargo:rerun-if-changed=cbindgen.toml");
+
     let bindings = cbindgen::generate(std::path::Path::new(".")).map_err(|e| e.to_string())?;
     // This uses unwraps internally; it'd be good to submit a patch
     // to add a Result-based API.
