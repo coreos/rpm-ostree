b624c5f47a88c4ca5808b9203b8f1b3051114456
Colin Walters <walters@verbum.org>
2018-10-09 17:32:09 +0000

    fixup! compose: Support not specifying a ref

--- tests/compose-tests/test-write-commitid.sh
+++ tests/compose-tests/test-write-commitid.sh
@@ -6,8 +6,8 @@ dn=$(cd $(dirname $0) && pwd)
 . ${dn}/libcomposetest.sh
 
 prepare_compose_test "write-commitid"
-runcompose --write-commitid-to $(pwd)/commitid.txt \
-           --write-composejson-to $(pwd)/composemeta.json
+treeref=""
+runcompose --write-commitid-to $(pwd)/commitid.txt
 wc -c < commitid.txt > wc.txt
 assert_file_has_content_literal wc.txt 64
 echo "ok compose"
@@ -19,11 +19,10 @@ fi
 echo "ok ref not written"
 
 commitid_txt=$(cat commitid.txt)
-json_commit=$(jq -r '.["ostree-commit"]' composemeta.json)
-assert_streq "${json_commit}" "${commitid_txt}"
+assert_streq "${commit}" "${commitid_txt}"
 # And verify we have other keys
 for key in ostree-version rpm-ostree-inputhash ostree-content-bytes-written; do
-    jq -r '.["'${key}'"]' composemeta.json >/dev/null
+    jq -r '.["'${key}'"]' ${composejson} >/dev/null
 done
 
 echo "ok composejson"
