f18d62d3cb401b870155f231b47d6ea4ee67f20d
Jonathan Lebon <jonathan@jlebon.com>
2019-09-06 15:25:27 -0400

    packaging: Move canonical spec file here
    So this is a somewhat significant change, but I'd like to try having the
    canonical spec file upstream. A few reasons for this:
    
    1. We integrate tightly with the distros we're destined for, and so
       we're in a pretty good position for knowing how the software should
       be packaged.
    2. We can atomically change packaging along with the rest of the code.
       This has important ramifications, including that it'll be easier to
       integrate with continuous build services like Packit, but releases
       will also be less fraught with last-minute packaging fixes.
    3. I'm playing with Jenkins pipelines and there I'd like to make RPMs
       the "artifact" that gets moved down the pipeline into later stages
       (e.g. `cosa build`). We could even eventually make it an actual
       external artifact so that anyone can easily download RPMs from any
       random PR for testing. (And in fact, with a thin yumrepo layer on
       top, it could be used to replace Packit/rdgo entirely).
    
    Not that this approach doesn't have issues as well (e.g. on the dist-git
    side, we'll need some minimal tooling to merge in the changelog), though
    I think it's worth trying out.

--- ci/installdeps.sh
+++ ci/installdeps.sh
@@ -22,6 +22,8 @@ fi
 
 pkg_upgrade
 pkg_install_builddeps rpm-ostree
+# and we have the canonical spec file handy so just builddep from that too
+dnf builddep --spec -y packaging/rpm-ostree.spec.in
 # Mostly dependencies for tests
 pkg_install ostree{,-devel,-grub2} createrepo_c /usr/bin/jq python3-pyyaml \
     libubsan libasan libtsan elfutils fuse sudo python3-gobject-base \
