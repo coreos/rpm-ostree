4b15c59b77c4d5e0d6a09200131c55efdfbb2ffa
Jonathan Lebon <jonathan@jlebon.com>
2020-02-21 22:38:54 -0500

    ci: Move `cargo test` into `ci/msrv.sh`
    We need `cargo` in our `PATH` and we already do the `PATH=...` dance in
    `ci/msrv.sh`. This only worked before because we were inadvertedly
    re-installing cargo when calling `ci/build.sh`, which was fixed in the
    previous commit.

--- ci/msrv.sh
+++ ci/msrv.sh
@@ -7,7 +7,12 @@ MINIMUM_SUPPORTED_RUST_VERSION=1.39.0
 ci/installdeps.sh
 dnf remove -y cargo
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain ${MINIMUM_SUPPORTED_RUST_VERSION} -y
-PATH="$HOME/.cargo/bin:$PATH" SKIP_INSTALLDEPS=1 ci/build.sh |& tee out.txt
+export PATH="$HOME/.cargo/bin:$PATH"
+SKIP_INSTALLDEPS=1 ci/build.sh |& tee out.txt
 grep ${MINIMUM_SUPPORTED_RUST_VERSION} out.txt
 grep "checking for cargo... $HOME/.cargo/bin/cargo" out.txt
 grep "checking for rustc... $HOME/.cargo/bin/rustc" out.txt
+
+# and run the tests
+cd rust
+cargo test
