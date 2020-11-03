coreos/rpm-ostree

commit 7f579a55d3fb7ec1cb9f74f8ec6bc36675df2ccc

Use ostree-rs, and oxidize more treefile checksumming
The main goal here is simply to start the ball rolling
in having rpm-ostree use the ostree-rs crate, so we can
use it for other code in the future.  We also bump the
glib crate to a new major, but no code changes were
required for that.

I spent a bit of time looking through our C code for
an example to oxidize, but almost all of our C code using
libostree also has calls back into other nontrivial rpm-ostree
C code.  To make a lot more oxidation progress I think we need
to invent an internal `librpmostree-sys` crate so we can have
Rust -> C calls.

I found this bit of treefile code computing the checksum that
we can oxidize - and as soon as we have more libdnf bindings
we can move all of the checksumming into Rust too.

rust/src/treefile.rs
@@ -488,6 +488,24 @@ impl Treefile {
         ))
     }
 
+    fn update_checksum(&self, repo: &ostree::Repo, checksum: &mut glib::Checksum) -> Result<()> {
+        let it = self.parsed.ostree_layers.iter().flat_map(|x| x.iter());
+        let it = it.chain(
+            self.parsed
+                .ostree_override_layers
+                .iter()
+                .flat_map(|x| x.iter()),
+        );
+        for v in it {
+            let rev = repo.resolve_rev(v, false)?;
+            let (commit, _) = repo.load_commit(rev.as_str())?;
+            let content_checksum =
+                ostree::commit_get_content_checksum(&commit).expect("content checksum");
+            checksum.update(content_checksum.as_bytes());
+        }
+        Ok(())
+    }
+
     /// Generate a rojig spec file.
     fn write_rojig_spec<'a, 'b>(workdir: &'a openat::Dir, r: &'b Rojig) -> Result<CUtf8Buf> {
         let description = r
@@ -1442,6 +1460,19 @@ mod ffi {
             ret.extend(layers.iter().cloned())
         }
         ret.to_glib_full()
+    }
+
+    #[no_mangle]
+    pub extern "C" fn ror_treefile_update_checksum(
+        tf: *mut Treefile,
+        repo: *mut ostree_sys::OstreeRepo,
+        checksum: *mut glib_sys::GChecksum,
+        gerror: *mut *mut glib_sys::GError,
+    ) -> libc::c_int {
+        let repo: ostree::Repo = unsafe { from_glib_none(repo) };
+        let mut checksum: glib::Checksum = unsafe { from_glib_none(checksum) };
+        let tf = ref_from_raw_ptr(tf);
+        int_glib_error(tf.update_checksum(&repo, &mut checksum), gerror)
     }
 
     #[no_mangle]

src/app/rpmostree-composeutil.c
@@ -56,6 +56,7 @@ rpmostree_composeutil_checksum (HyGoal             goal,
                                 char             **out_checksum,
                                 GError           **error)
 {
+  GLNX_AUTO_PREFIX_ERROR ("Computing compose checksum", error);
   g_autoptr(GChecksum) checksum = g_checksum_new (G_CHECKSUM_SHA256);
 
   /* Hash in the treefile inputs (this includes all externals like postprocess, add-files,
@@ -68,20 +69,8 @@ rpmostree_composeutil_checksum (HyGoal             goal,
 
   if (tf)
     {
-      GLNX_AUTO_PREFIX_ERROR ("Resolving ostree-layers", error);
-      g_auto(GStrv) layers = ror_treefile_get_all_ostree_layers (tf);
-      for (char **iter = layers; iter && *iter; iter++)
-        {
-          const char *layer = *iter;
-          g_autofree char *rev = NULL;
-          if (!ostree_repo_resolve_rev (repo, layer, FALSE, &rev, error))
-            return FALSE;
-          g_autoptr(GVariant) commit = NULL;
-          if (!ostree_repo_load_commit (repo, rev, &commit, NULL, error))
-            return FALSE;
-          const char *content_checksum = ostree_commit_get_content_checksum (commit);
-          g_checksum_update (checksum, (const guint8*) content_checksum, strlen (content_checksum));
-        }
+      if (!ror_treefile_update_checksum (tf, repo, checksum, error))
+        return FALSE;
     }
 
   *out_checksum = g_strdup (g_checksum_get_string (checksum));

rust/Cargo.toml
@@ -12,9 +12,12 @@ serde_json = "1.0"
 serde_yaml = "0.8"
 libc = "0.2"
 nix = "0.18.0"
-glib-sys = "0.9.1"
-gio-sys = "0.9.1"
-glib = "0.9"
+glib-sys = "0.10.0"
+glib = "0.10"
+gio-sys = "0.10.0"
+gio = "0.9"
+ostree = { version = "0.9.0", features = ["v2020_4"] }
+ostree-sys = "0.7.0"
 tempfile = "3.0.3"
 clap = "~2.33"
 structopt = "0.3"

src/libpriv/rpmostree-rust-prelude.h
@@ -29,4 +29,6 @@
 b(GError)
 b(GHashTable)
 b(GPtrArray)
+b(GChecksum)
+b(OstreeRepo)
 #undef b

commit 1a9eda548dc2e767bbe40b44ecad3e161851c322

treefile: Fix hashing of externals
See https://github.com/coreos/rpm-ostree/pull/2206#issuecomment-721372634

The commit 7f579a55d3fb7ec1cb9f74f8ec6bc36675df2ccc broke hashing
of overlay commits; this is a super evil bug because it causes us
to silently do the wrong thing.

The cause here is the GLib bindings don't (AFAICS) support getting
a `&mut` for a GLib boxed value.

Move all of the treefile checksum code into one place - this is
far saner.  The reason I didn't do this before is that it
will cause a spurious rebuild when one updates rpm-ostree, but...eh.

rust/src/treefile.rs
@@ -45,8 +45,6 @@ pub struct Treefile {
     rojig_spec: Option<CUtf8Buf>,
     serialized: CUtf8Buf,
     externals: TreefileExternals,
-    // This is a checksum over *all* the treefile inputs (recursed treefiles + externals).
-    checksum: CUtf8Buf,
 }
 
 // We only use this while parsing
@@ -433,13 +431,6 @@ impl Treefile {
             _ => (None, None),
         };
         let serialized = Treefile::serialize_json_string(&parsed.config)?;
-        // Notice we hash the *reserialization* of the final flattened treefile only so that e.g.
-        // comments/whitespace/hash table key reorderings don't trigger a respin. We could take
-        // this further by using a custom `serialize_with` for Vecs where ordering doesn't matter
-        // (or just sort the Vecs).
-        let mut hasher = glib::Checksum::new(glib::ChecksumType::Sha256);
-        parsed.config.hasher_update(&mut hasher)?;
-        parsed.externals.hasher_update(&mut hasher)?;
         Ok(Box::new(Treefile {
             primary_dfd: dfd,
             parsed: parsed.config,
@@ -448,7 +439,6 @@ impl Treefile {
             rojig_spec,
             serialized,
             externals: parsed.externals,
-            checksum: CUtf8Buf::from_string(hasher.get_string().unwrap()),
         }))
     }
 
@@ -511,7 +501,15 @@ impl Treefile {
         }
     }
 
-    fn update_checksum(&self, repo: &ostree::Repo, checksum: &mut glib::Checksum) -> Result<()> {
+    fn get_checksum(&self, repo: &ostree::Repo) -> Result<String> {
+        // Notice we hash the *reserialization* of the final flattened treefile only so that e.g.
+        // comments/whitespace/hash table key reorderings don't trigger a respin. We could take
+        // this further by using a custom `serialize_with` for Vecs where ordering doesn't matter
+        // (or just sort the Vecs).
+        let mut hasher = glib::Checksum::new(glib::ChecksumType::Sha256);
+        self.parsed.hasher_update(&mut hasher)?;
+        self.externals.hasher_update(&mut hasher)?;
+
         let it = self.parsed.ostree_layers.iter().flat_map(|x| x.iter());
         let it = it.chain(
             self.parsed
@@ -519,14 +517,17 @@ impl Treefile {
                 .iter()
                 .flat_map(|x| x.iter()),
         );
+
         for v in it {
             let rev = repo.resolve_rev(v, false)?;
-            let (commit, _) = repo.load_commit(rev.as_str())?;
+            let rev = rev.as_str();
+            let (commit, _) = repo.load_commit(rev)?;
             let content_checksum =
                 ostree::commit_get_content_checksum(&commit).expect("content checksum");
-            checksum.update(content_checksum.as_bytes());
+            let content_checksum = content_checksum.as_str();
+            hasher.update(content_checksum.as_bytes());
         }
-        Ok(())
+        Ok(hasher.get_string().expect("hash"))
     }
 
     /// Generate a rojig spec file.
@@ -1507,19 +1508,6 @@ mod ffi {
     }
 
     #[no_mangle]
-    pub extern "C" fn ror_treefile_update_checksum(
-        tf: *mut Treefile,
-        repo: *mut ostree_sys::OstreeRepo,
-        checksum: *mut glib_sys::GChecksum,
-        gerror: *mut *mut glib_sys::GError,
-    ) -> libc::c_int {
-        let repo: ostree::Repo = unsafe { from_glib_none(repo) };
-        let mut checksum: glib::Checksum = unsafe { from_glib_none(checksum) };
-        let tf = ref_from_raw_ptr(tf);
-        int_glib_error(tf.update_checksum(&repo, &mut checksum), gerror)
-    }
-
-    #[no_mangle]
     pub extern "C" fn ror_treefile_get_rojig_spec_path(tf: *mut Treefile) -> *const libc::c_char {
         let tf = ref_from_raw_ptr(tf);
         if let &Some(ref rojig) = &tf.rojig_spec {
@@ -1539,9 +1527,20 @@ mod ffi {
     }
 
     #[no_mangle]
-    pub extern "C" fn ror_treefile_get_checksum(tf: *mut Treefile) -> *const libc::c_char {
+    pub extern "C" fn ror_treefile_get_checksum(
+        tf: *mut Treefile,
+        repo: *mut ostree_sys::OstreeRepo,
+        gerror: *mut *mut glib_sys::GError,
+    ) -> *mut libc::c_char {
         let tf = ref_from_raw_ptr(tf);
-        tf.checksum.as_ptr()
+        let repo: ostree::Repo = unsafe { from_glib_none(repo) };
+        match tf.get_checksum(&repo) {
+            Ok(c) => c.to_glib_full(),
+            Err(ref e) => {
+                error_to_glib(e, gerror);
+                ptr::null_mut()
+            }
+        }
     }
 
     #[no_mangle]

src/app/rpmostree-composeutil.c
@@ -61,17 +61,14 @@ rpmostree_composeutil_checksum (HyGoal             goal,
 
   /* Hash in the treefile inputs (this includes all externals like postprocess, add-files,
    * etc... and the final flattened treefile -- see treefile.rs for more details). */
-  g_checksum_update (checksum, (guint8*)ror_treefile_get_checksum (tf), -1);
+  g_autofree char *tf_checksum = ror_treefile_get_checksum (tf, repo, error);
+  if (!tf_checksum)
+    return FALSE;
+  g_checksum_update (checksum, (guint8*) tf_checksum, -1);
 
   /* Hash in each package */
   if (!rpmostree_dnf_add_checksum_goal (checksum, goal, NULL, error))
     return FALSE;
-
-  if (tf)
-    {
-      if (!ror_treefile_update_checksum (tf, repo, checksum, error))
-        return FALSE;
-    }
 
   *out_checksum = g_strdup (g_checksum_get_string (checksum));
   return TRUE;