56760352427db216fdf6a9fa5cf67fba9fc65534
Colin Walters <walters@verbum.org>
2020-12-15 21:45:10 +0000

    shlib: Make basearch APIs call `/usr/bin/rpm-ostree shlib-backend`
    This is a potential path to fix
    https://github.com/coreos/rpm-ostree/issues/2391
    
    Basically our shared library and executable duplicate too much
    code (particularly Rust).  Since most of the shlib APIs aren't
    performance sensitive, let's have them fork off the binary
    via a hidden CLI entrypoint and parse the output.

---
+++ src/app/rpmostree-builtin-shlib-backend.c
@@ -0,0 +1,107 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright (C) 2020 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2 of the licence or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include <string.h>
+#include <glib-unix.h>
+#include <gio/gio.h>
+#include <gio/gunixfdmessage.h>
+#include <gio/gunixsocketaddress.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "rpmostree-builtins.h"
+#include "rpmostree-libbuiltin.h"
+#include "rpmostree-rust.h"
+#include "rpmostree.h"
+#include "rpmostree-core.h"
+#include "src/lib/rpmostree-shlib-ipc-private.h"
+
+#include <libglnx.h>
+
+static gboolean
+send_memfd_result (int ret_memfd, GError **error)
+{
+  int fdarray[] = {ret_memfd, -1 };
+  g_autoptr(GUnixFDList) list = g_unix_fd_list_new_from_array (fdarray, 1);
+  g_autoptr(GUnixFDMessage) message = G_UNIX_FD_MESSAGE (g_unix_fd_message_new_with_fd_list (list));
+  g_autoptr(GSocket) ipc_sock = g_socket_new_from_fd (RPMOSTREE_SHLIB_IPC_FD, error);
+  if (!ipc_sock)
+    return FALSE;
+  GOutputVector ov;
+  char buffer[1];
+  buffer[0] = 0xFF;
+  ov.buffer = buffer;
+  ov.size = G_N_ELEMENTS (buffer);
+  gssize r = g_socket_send_message (ipc_sock, NULL, &ov, 1,
+                                    (GSocketControlMessage **) &message,
+                                    1, 0, NULL, error);
+  if (r < 0)
+    return FALSE;
+  g_assert_cmpint (r, ==, 1);
+
+  return TRUE;
+}
+
+gboolean
+rpmostree_builtin_shlib_backend (int             argc,
+                                 char          **argv,
+                                 RpmOstreeCommandInvocation *invocation,
+                                 GCancellable   *cancellable,
+                                 GError        **error)
+{
+  if (argc < 2)
+    return glnx_throw (error, "missing required subcommand");
+
+  const char *arg = argv[1];
+
+  g_autoptr(GVariant) ret = NULL;
+
+  if (g_str_equal (arg, "get-basearch"))
+    {
+      g_autoptr(DnfContext) ctx = dnf_context_new ();
+      ret = g_variant_new_string (dnf_context_get_base_arch (ctx));
+    }
+  else if (g_str_equal (arg, "varsubst-basearch"))
+    {
+      const char *src = argv[2];
+      g_autoptr(DnfContext) ctx = dnf_context_new ();
+      g_autoptr(GHashTable) varsubsts = rpmostree_dnfcontext_get_varsubsts (ctx);
+      g_autofree char *rets = _rpmostree_varsubst_string (src, varsubsts, error);
+      if (rets == NULL)
+        return FALSE;
+      ret = g_variant_new_string (rets);
+    }
+  else
+    return glnx_throw (error, "unknown shlib-backend %s", arg);
+
+  glnx_fd_close int ret_memfd = memfd_create ("rpm-ostree-shlib-backend", MFD_CLOEXEC | MFD_ALLOW_SEALING);
+  if (ret_memfd < 0)
+    return glnx_throw_errno_prefix (error, "memfd_create");
+  if (glnx_loop_write (ret_memfd, g_variant_get_data (ret), g_variant_get_size (ret)) < 0)
+    return glnx_throw_errno_prefix (error, "Failed to write to memfd");
+  const int seals = (F_SEAL_GROW | F_SEAL_SHRINK | F_SEAL_WRITE);
+  if (fcntl (ret_memfd, F_ADD_SEALS, seals) == -1)
+    return glnx_throw_errno_prefix (error, "fcntl(sealing)");
+
+  return send_memfd_result (glnx_steal_fd (&ret_memfd), error);
+}
