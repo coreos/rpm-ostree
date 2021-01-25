7cff17ecb5f4ae8f33613b809fe8a9db017b9213
Kelvin Fan <kfan@redhat.com>
2021-01-25 18:00:03 -0500

    deploy: Allow empty string argument if `--register-driver`
    It is sometimes useful to only register an update driver without
    actually deploying anything. If the argument for `deploy` is an
    empty string, only register driver and then no-op.

--- src/app/rpmostree-builtin-deploy.cxx
+++ src/app/rpmostree-builtin-deploy.cxx
@@ -50,7 +50,7 @@ static GOptionEntry option_entries[] = {
   { "lock-finalization", 0, G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &opt_lock_finalization, "Prevent automatic deployment finalization on shutdown", NULL },
   { "disallow-downgrade", 0, 0, G_OPTION_ARG_NONE, &opt_disallow_downgrade, "Forbid deployment of chronologically older trees", NULL },
   { "unchanged-exit-77", 0, 0, G_OPTION_ARG_NONE, &opt_unchanged_exit_77, "If no new deployment made, exit 77", NULL },
-  { "register-driver", 0, 0, G_OPTION_ARG_STRING, &opt_register_driver, "Register the calling agent as the driver for updates. Takes a human-readable string as name for driver", "DRIVERNAME" },
+  { "register-driver", 0, 0, G_OPTION_ARG_STRING, &opt_register_driver, "Register the calling agent as the driver for updates; if REVISION is an empty string, register driver without deploying", "DRIVERNAME" },
   { NULL }
 };
 
