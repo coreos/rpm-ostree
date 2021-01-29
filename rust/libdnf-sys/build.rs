588541c60de4a5dbf9d32ab99de628b718e491cc
Colin Walters <walters@verbum.org>
2021-01-29 19:47:48 +0000

    Move libdnf build over to Cargo
    This is now further migration towards Cargo/Rust possible
    because we switched our main binary.  We've had an internal
    `libdnf-sys` crate for a while, but now it can take over
    the build of the underlying library too (like many `-sys`
    crates support).
    
    This itself is just an incremental step towards migrating
    the main rpm-ostree build system to e.g. cmake too (or
    perhaps directly with the `cc` crate, not sure yet) and
    driving it via `cargo` too.

--- rust/libdnf-sys/build.rs
+++ rust/libdnf-sys/build.rs
@@ -2,5 +2,36 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     system_deps::Config::new().probe()?;
+    use cmake::Config;
+    let libdnf = Config::new("../../libdnf")
+        // Needed for hardened builds
+        .cxxflag("-fPIC")
+        // I picked /usr/libexec/rpm-ostree just because we need an
+        // arbitrary path - we don't actually install there.
+        .define("CMAKE_INSTALL_PREFIX:PATH", "/usr/libexec/rpm-ostree")
+        .define(
+            "INCLUDE_INSTALL_DIR:PATH",
+            "/usr/libexec/rpm-ostree/include",
+        )
+        .define("LIB_INSTALL_DIR:PATH", "/usr/libexec/rpm-ostree")
+        .define("SYSCONF_INSTALL_DIR:PATH", "/usr/libexec/rpm-ostree/etc")
+        .define("SHARE_INSTALL_PREFIX:PATH", "/usr/libexec/rpm-ostree/share")
+        .define("ENABLE_STATIC:BOOL", "1")
+        .define("CMAKE_POSITION_INDEPENDENT_CODE", "ON")
+        // rpm-ostree maintains its own state
+        .define("WITH_SWDB:BOOL", "0")
+        // We don't need docs
+        .define("WITH_HTML:BOOL", "0")
+        .define("WITH_MAN:BOOL", "0")
+        // Don't need bindings
+        .define("WITH_BINDINGS:BOOL", "0")
+        .define("WITH_GIR:BOOL", "0")
+        .build_target("all")
+        .build();
+    println!(
+        "cargo:rustc-link-search=native={}/build/libdnf",
+        libdnf.display()
+    );
+    println!("cargo:rustc-link-lib=static=dnf");
     Ok(())
 }
