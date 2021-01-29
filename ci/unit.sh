cd9f5256bae35681bb6738d1c19e636a1737be2d
Colin Walters <walters@verbum.org>
2021-01-29 17:54:07 +0000

    ci: Drop custom msrv checking
    The way this tries to replace the system Rust is hacky and
    actually I realized belatedly I may have broken it recently; basically
    `installdeps.sh` re-adds the system one, and it's hard to be sure
    with our current buildsystem we're using the newer one from `$PATH`.
    
    What we really want to do here is use a CentOS8 buildroot,
    which will automatically enforce this in a better way along
    with solving other problems.  But right now we've broken
    that because libdnf requires a too-new libmodulemd.
    
    So let's just rely on the Fedora rust for now.

--- ci/unit.sh
+++ ci/unit.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# Usually, we try to keep this to no newer than current RHEL8 rust-toolset version.
-# You can find the current versions from here:
-# https://access.redhat.com/documentation/en-us/red_hat_developer_tools/1/
-# However, right now we are bumping to 1.48 so we can use https://cxx.rs
-MINIMUM_SUPPORTED_RUST_VERSION=1.48
-
 ci/installdeps.sh
-dnf remove -y cargo rust
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain ${MINIMUM_SUPPORTED_RUST_VERSION} -y
+ci/install-extra-builddeps.sh
 export PATH="$HOME/.cargo/bin:$PATH"
 ci/build.sh
-cargo +${MINIMUM_SUPPORTED_RUST_VERSION} test
