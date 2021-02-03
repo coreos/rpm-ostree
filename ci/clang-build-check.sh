7268ac98759dabc6701d36b5f544b94fccf64516
Colin Walters <walters@verbum.org>
2021-02-03 20:54:24 +0000

    ci: Consistently source libbuild
    Since we need to set HOME and PATH, let's do that in a central
    place rather than scattering it around by having all of
    our entrypoint scripts source the `libbuild.sh` shell "library".
    
    Move the CoreOS CI entrypoint into a script like the others.

--- ci/clang-build-check.sh
+++ ci/clang-build-check.sh
@@ -7,10 +7,6 @@ set -xeuo pipefail
 
 dn=$(dirname $0)
 . ${dn}/libbuild.sh
-
-# add cargo's directory to the PATH like we do in CoreOS CI
-export PATH="$HOME/.cargo/bin:$PATH"
-
 export CC=clang CXX=clang++
 ${dn}/build.sh
 make check
--- ci/unit.sh
+++ ci/unit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+dn=$(dirname $0)
+. ${dn}/libbuild.sh
+
 ci/installdeps.sh
 ci/install-extra-builddeps.sh
-export PATH="$HOME/.cargo/bin:$PATH"
 ci/build.sh
