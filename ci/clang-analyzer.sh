62795824528df16d2052084017ff0ecb0fe75f39
Colin Walters <walters@verbum.org>
2021-02-05 13:49:48 +0000

    ci: Add clang-analyzer.sh
    There are really no excuses for any C/C++ project not to use
    both ASAN+UBSAN and static analysis in CI.

---
+++ ci/clang-analyzer.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# Use the clang static analyzer
+
+set -xeuo pipefail
+
+dn=$(dirname $0)
+. ${dn}/libbuild.sh
+env NOCONFIGURE=1 ./autogen.sh
+scan-build ./configure --prefix=/usr --libdir=/usr/lib64 --sysconfdir=/etc
+scan-build ${ARTIFACT_DIR:+-o ${ARTIFACT_DIR}} make
