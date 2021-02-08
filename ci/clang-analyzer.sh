b942bd8cf7d519b9c36852c819180c736da7d44e
Colin Walters <walters@verbum.org>
2021-02-08 17:15:01 +0000

    ci/clang-analyzer: Install deps
    This flow was missing from here which broke in CI.

--- ci/clang-analyzer.sh
+++ ci/clang-analyzer.sh
@@ -5,6 +5,8 @@ set -xeuo pipefail
 
 dn=$(dirname $0)
 . ${dn}/libbuild.sh
+${dn}/install-extra-builddeps.sh
+${dn}/installdeps.sh
 env NOCONFIGURE=1 ./autogen.sh
 scan-build ./configure --prefix=/usr --libdir=/usr/lib64 --sysconfdir=/etc
 scan-build ${ARTIFACT_DIR:+-o ${ARTIFACT_DIR}} make
