a0e2dd06c4e245e1207878bdd37ca87e75e2bc08
Colin Walters <walters@verbum.org>
2021-02-16 15:01:29 +0000

    ci: Drop: distro-sync, continuous repo
    Doing the distro-sync costs ~3 minutes per execution, and it
    happens multiple times.  Let's just ensure our images are up
    to date instead.
    
    Also drop the continuous repo (for now) - we added this
    to test bleeding edge ostree, but I think we need to reintroduce
    "git master" builds as whole separate CI flow (layered container)
    instead.

--- ci/installdeps.sh
+++ ci/installdeps.sh
@@ -10,17 +10,11 @@ if [ -n "${SKIP_INSTALLDEPS:-}" ] || test $(id -u) != 0; then
     exit 0
 fi
 
-# Add the continuous tag for latest build tools and mark as required.
-version_id=$(. /etc/os-release && echo $VERSION_ID)
-echo -e "[f${version_id}-coreos-continuous]\nenabled=1\nmetadata_expire=1m\nbaseurl=https://kojipkgs.fedoraproject.org/repos-dist/f${version_id}-coreos-continuous/latest/\$basearch/\ngpgcheck=0\nskip_if_unavailable=False\n" > /etc/yum.repos.d/coreos.repo
-
-pkg_upgrade
-# install base builddeps like @buildsys-build
-pkg_install_builddeps
 # we have the canonical spec file handy so just builddep from that
 # XXX: use --allowerasing as a temporary hack to ease the migration to libmodulemd2
 dnf builddep --spec -y packaging/rpm-ostree.spec.in --allowerasing
-# Mostly dependencies for tests
+# Mostly dependencies for tests; TODO move these into the spec file
+# and also put them in the cosa buildroot (or another container)
 pkg_install ostree{,-devel,-grub2} createrepo_c /usr/bin/jq python3-pyyaml \
     libubsan libasan libtsan elfutils fuse sudo python3-gobject-base \
     selinux-policy-devel selinux-policy-targeted python3-createrepo_c \
