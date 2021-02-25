1dc7503838b3cbb49b05322fd6fbad4a96d8fb68
Colin Walters <walters@verbum.org>
2021-02-25 17:33:14 +0000

    ci/prow: Install in target cosa, also install and run our tests
    This is a major downside of reworking and generating new CI
    flows, it's super easy to lose testing what you intend to.
    
    Also, we clearly need to figure out a flow where this is shared
    across repos, since I don't want to copy-paste this into e.g. ostree too.
    That's https://github.com/coreos/fedora-coreos-tracker/issues/263

--- ci/prow/fcos-e2e.sh
+++ ci/prow/fcos-e2e.sh
@@ -3,9 +3,13 @@ set -xeuo pipefail
 
 # Prow jobs don't support adding emptydir today
 export COSA_SKIP_OVERLAY=1
-gitdir=$(pwd)
+# And suppress depcheck since we didn't install via RPM
+export COSA_SUPPRESS_DEPCHECK=1
+ls -al /usr/bin/rpm-ostree
+rpm-ostree --version
 cd $(mktemp -d)
-cosa init --force https://github.com/coreos/fedora-coreos-config/
+cosa init https://github.com/coreos/fedora-coreos-config/
+rsync -rlv /cosa/component-install/ overrides/rootfs/
 cosa fetch
 cosa build
-cosa kola run 'ext.*'
+cosa kola run 'ext.rpm-ostree.*'
