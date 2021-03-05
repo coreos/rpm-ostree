14c17cfe527801b51e94905fa3e89a046fd91aa0
Colin Walters <walters@verbum.org>
2021-03-05 18:11:03 +0000

    rust/client: Add methods to find/require booted deployment
    This is a common need.

--- rust/rpmostree-client/tests/parse.rs
+++ rust/rpmostree-client/tests/parse.rs
@@ -7,7 +7,7 @@ fn parse_workstation() -> Result<()> {
     let data = include_str!("fixtures/workstation-status.json");
     let state: &rpmostree_client::Status = &serde_json::from_str(data)?;
     assert_eq!(state.deployments.len(), 2);
-    let booted = &state.deployments[0];
+    let booted = state.require_booted().unwrap();
     assert_eq!(booted.version.as_ref().unwrap().as_str(), "33.21");
     Ok(())
 }
