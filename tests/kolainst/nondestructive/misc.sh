445af087d618bbb77437d710af4abb89697e6d06
Colin Walters <walters@verbum.org>
2021-03-11 14:55:27 +0000

    tests: Drain vmcheck/test-misc-1 into kola/misc.sh
    Where I stalled out before is this file has `pkg-add foo`, but
    now that we have the `foo` package pre-built we can move all
    this stuff into `misc.sh`.
    
    I dropped the YAML parsing of `--version` because we don't
    have python.  This is related to
    https://github.com/coreos/coreos-assembler/issues/1645

--- tests/kolainst/nondestructive/misc.sh
+++ tests/kolainst/nondestructive/misc.sh
@@ -38,6 +38,37 @@ echo "ok jsonpath"
 runuser -u core rpm-ostree status
 echo "ok status doesn't require root"
 
+if runuser -u core rpm-ostree pkg-add foo &>err.txt; then
+    fatal "Was able to install a package as non-root!"
+fi
+assert_file_has_content err.txt 'PkgChange not allowed for user'
+if runuser -u core rpm-ostree reload &>err.txt; then
+    assert_not_reached "Was able to reload as non-root!"
+fi
+echo "ok polkit"
+
+wrapdir="/usr/libexec/rpm-ostree/wrapped"
+if [ -d "${wrapdir}" ]; then
+    # Test wrapped functions for rpm
+    rpm --version
+    rpm -qa > /dev/null
+    rpm --verify >out.txt
+    assert_file_has_content out.txt "rpm --verify is not necessary for ostree-based systems"
+    rm -f out.txt
+    if rpm -e bash 2>out.txt; then
+        fatal "rpm -e worked"
+    fi
+    assert_file_has_content out.txt 'Dropping privileges as `rpm` was executed with not "known safe" arguments'
+
+    if dracut --blah 2>out.txt; then
+        fatal "dracut worked"
+    fi
+    assert_file_has_content out.txt 'This system is rpm-ostree based'
+    rm -f out.txt
+else
+    echo "Missing ${wrapdir}; cliwrap not enabled"
+fi
+
 # StateRoot is only in --verbose
 rpm-ostree status > status.txt
 assert_not_file_has_content status.txt StateRoot:
@@ -75,3 +106,32 @@ echo "ok crypto-policies DEFAULT backend"
 ldd /usr/lib64/librpmostree-1.so.1 > rpmostree-lib-deps.txt
 assert_not_file_has_content rpmostree-lib-deps.txt libdnf
 echo "ok lib deps"
+
+mv /etc/ostree/remotes.d{,.orig}
+systemctl restart rpm-ostreed
+rpm-ostree status > status.txt
+assert_file_has_content status.txt 'Remote.*not found'
+mv /etc/ostree/remotes.d{.orig,}
+rpm-ostree reload
+echo "ok remote not found"
+
+rpm-ostree cleanup -p
+originpath=$(ostree admin --print-current-dir).origin
+unshare -m /bin/bash -c "mount -o remount,rw /sysroot && cp -a ${originpath}{,.orig} && 
+   echo 'unconfigured-state=Access to TestOS requires ONE BILLION DOLLARS' >> ${originpath}"
+rpm-ostree reload
+rpm-ostree status
+if rpm-ostree upgrade 2>err.txt; then
+    echo "Upgraded from unconfigured-state"
+    exit 1
+fi
+grep -qFe 'ONE BILLION DOLLARS' err.txt
+unshare -m /bin/bash -c "mount -o remount,rw /sysroot && cp -a ${originpath}{.orig,}"
+rpm-ostree reload
+echo "ok unconfigured-state"
+
+### Stuff following here may mutate the host persistently ###
+
+rpm-ostree usroverlay
+echo some content > /usr/share/testcontent
+echo "ok usroverlay"
