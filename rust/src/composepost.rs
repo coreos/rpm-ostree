f1138dd134903eecb9b05fb8f5f20ca1378304af
Luca BRUNO <luca.bruno@coreos.com>
2021-04-22 10:44:00 +0000

    composepost: move SELinux workaround logic to Rust
    This ports the timestamp updating logic which serves as a workaround
    for SELinux leaking host details.

--- rust/src/composepost.rs
+++ rust/src/composepost.rs
@@ -770,6 +770,89 @@ fn ensure_symlink(rootfs: &openat::Dir, target: &str, linkpath: &str) -> Result<
     Ok(true)
 }
 
+pub fn workaround_selinux_cross_labeling(
+    rootfs_dfd: i32,
+    mut cancellable: Pin<&mut crate::FFIGCancellable>,
+) -> CxxResult<()> {
+    let rootfs = crate::ffiutil::ffi_view_openat_dir(rootfs_dfd);
+    let cancellable = &cancellable.gobj_wrap();
+
+    tweak_selinux_timestamps(&rootfs, Some(cancellable))?;
+    Ok(())
+}
+
+/// Tweak timestamps on SELinux policy (workaround cross-host leak).
+///
+/// SELinux uses PCRE pre-compiled regexps for binary caches, which can
+/// fail if the version of PCRE on the host differs from the version
+/// which generated the cache (in the target root).
+///
+/// Note also this function is probably already broken in Fedora
+/// 23+ from https://bugzilla.redhat.com/show_bug.cgi?id=1265406
+#[context("Tweaking SELinux timestamps")]
+fn tweak_selinux_timestamps(
+    rootfs: &openat::Dir,
+    cancellable: Option<&gio::Cancellable>,
+) -> Result<()> {
+    // Handle the policy being in both /usr/etc and /etc since
+    // this function can be called at different points.
+    let policy_path = if rootfs.exists("usr/etc")? {
+        "usr/etc/selinux"
+    } else {
+        "etc/selinux"
+    };
+
+    if rootfs.exists(policy_path)? {
+        let mut prefix = policy_path.to_string();
+        workaround_selinux_cross_labeling_recurse(rootfs, &mut prefix, &cancellable)
+            .with_context(|| format!("Analyzing /{} content", prefix))?;
+    }
+
+    Ok(())
+}
+
+/// Recursively explore target directory and tweak SELinux policy timestamps.
+///
+/// `prefix` is updated at each recursive step, so that in case of errors it can be
+/// used to pinpoint the faulty path.
+fn workaround_selinux_cross_labeling_recurse(
+    rootfs: &openat::Dir,
+    prefix: &mut String,
+    cancellable: &Option<&gio::Cancellable>,
+) -> Result<()> {
+    use openat::SimpleType;
+
+    let current_prefix = prefix.clone();
+    for subpath in rootfs.list_dir(&current_prefix)? {
+        if cancellable.map(|c| c.is_cancelled()).unwrap_or_default() {
+            bail!("Cancelled");
+        };
+
+        let subpath = subpath?;
+        let full_path = {
+            let fname = subpath.file_name();
+            let path_name = fname
+                .to_str()
+                .ok_or_else(|| anyhow!("invalid non-UTF-8 path: {:?}", fname))?;
+            format!("{}/{}", &current_prefix, &path_name)
+        };
+        let path_type = subpath.simple_type().unwrap_or(SimpleType::Other);
+
+        if path_type == SimpleType::Dir {
+            // New subdirectory discovered, recurse into it.
+            *prefix = full_path.clone();
+            workaround_selinux_cross_labeling_recurse(rootfs, prefix, cancellable)?;
+        } else {
+            if let Some(nonbin_name) = full_path.strip_suffix(".bin") {
+                rootfs
+                    .update_timestamps(nonbin_name)
+                    .with_context(|| format!("Updating timestamps of /{}", nonbin_name))?;
+            }
+        }
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -956,6 +1039,45 @@ OSTREE_VERSION='33.4'
                 assert!(target.is_ok(), "/var/lib/{}", linkpath);
                 assert_eq!(target.unwrap().to_str(), Some(*content));
             }
+        }
+    }
+
+    #[test]
+    fn test_tweak_selinux_timestamps() {
+        static PREFIX: &str = "usr/etc/selinux/targeted/contexts/files";
+
+        let temp_rootfs = tempfile::tempdir().unwrap();
+        let rootfs = openat::Dir::open(temp_rootfs.path()).unwrap();
+        rootfs
+            .ensure_dir_all("usr/etc/selinux/targeted/contexts/files", 0o755)
+            .unwrap();
+        let files = &["file_contexts", "file_contexts.homedirs"];
+
+        let mut metas = vec![];
+        for fname in files {
+            let binpath = format!("{}/{}.bin", PREFIX, fname);
+            rootfs.new_file(&binpath, 0o755).unwrap();
+            let fpath = format!("{}/{}", PREFIX, fname);
+            rootfs.new_file(&fpath, 0o755).unwrap();
+            rootfs.syncfs().unwrap();
+            let before_meta = rootfs.metadata(fpath).unwrap();
+            metas.push(before_meta);
+        }
+
+        tweak_selinux_timestamps(&rootfs, gio::NONE_CANCELLABLE).unwrap();
+        rootfs.syncfs().unwrap();
+
+        for (fname, before_meta) in files.iter().zip(metas.iter()) {
+            let fpath = format!("{}/{}", PREFIX, fname);
+            let after = rootfs.metadata(&fpath).unwrap();
+            assert!(
+                before_meta.stat().st_atime != after.stat().st_atime
+                    || before_meta.stat().st_atime_nsec != after.stat().st_atime_nsec
+            );
+            assert!(
+                before_meta.stat().st_mtime != after.stat().st_mtime
+                    || before_meta.stat().st_mtime_nsec != after.stat().st_mtime_nsec
+            );
         }
     }
 }
