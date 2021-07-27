e34de348f4088c6a5291254eabdb3308345ae6b9
Colin Walters <walters@verbum.org>
2021-07-27 15:30:57 -0400

    Add .copr/Makefile
    Prep for re-introducing a coreos/continuous build.

--- ci/coreosci-rpmbuild.sh
+++ ci/coreosci-rpmbuild.sh
@@ -4,13 +4,6 @@ set -euo pipefail
 dn=$(dirname $0)
 . ${dn}/libbuild.sh
 
-# fetch tags so `git describe` gives a nice NEVRA when building the RPM
-git fetch origin --tags
-git submodule update --init --recursive
-ci/installdeps.sh
-# Our primary CI build goes via RPM rather than direct to binaries
-# to better test that path, including our vendored spec file, etc.
-# The RPM build expects pre-generated bindings, so do that now.
-make -f Makefile.bindings bindings
-cd packaging
-make -f Makefile.dist-packaging rpm
+make -f .copr/Makefile srpm
+./packaging/rpmbuild-cwd --rebuild packaging/*.src.rpm
+mv $(arch)/*.rpm .
