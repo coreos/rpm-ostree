5cf03f8534baaa407c5a84c8ecf1a6b17f6a978b
Colin Walters <walters@verbum.org>
2021-07-27 16:06:03 -0400

    ci: Install system deps before cxxbridge
    To ensure we have `jq` which we need; and this is required for COPR.

--- ci/installdeps.sh
+++ ci/installdeps.sh
@@ -6,21 +6,15 @@ set -xeuo pipefail
 dn=$(dirname $0)
 . ${dn}/libbuild.sh
 
+if [ -z "${SKIP_INSTALLDEPS:-}" ] && test $(id -u) -eq 0; then
+    dnf -y install jq dnf-plugins-core
+    # we have the canonical spec file handy so just builddep from that
+    # XXX: use --allowerasing as a temporary hack to ease the migration to libmodulemd2
+    time dnf builddep --spec -y packaging/rpm-ostree.spec.in --allowerasing
+fi
+
 # cxx.rs (cxxbridge) isn't packaged in Fedora today.  It generates
 # source code, which we vendor along with our dependent crates into release
 # tarballs.
 CXX_VER=$(cargo metadata --format-version 1 | jq -r '.packages[]|select(.name == "cxx").version')
 time cargo install cxxbridge-cmd --version "${CXX_VER}"
-
-# Everything below here uses dnf/yum; we don't try to use
-# sudo for you right now.  (Though hopefully you're building
-# in an unprivileged podman container at least)
-if [ -n "${SKIP_INSTALLDEPS:-}" ] || test $(id -u) != 0; then
-    exit 0
-fi
-
-# we have the canonical spec file handy so just builddep from that
-# XXX: use --allowerasing as a temporary hack to ease the migration to libmodulemd2
-time dnf builddep --spec -y packaging/rpm-ostree.spec.in --allowerasing
-
-
