baf28a9ab762552136ad3b27f0298725a0183adc
Colin Walters <walters@verbum.org>
2021-08-19 10:55:43 -0400

    Change our failpoint to `return Err` instead of panic
    Part of looking at https://github.com/coreos/rpm-ostree/issues/3075
    
    I thought there was an issue with us not catching a panic and
    the core dump handler being called before stderr was flushed.
    
    In the end, that wasn't the bug, but I think it's just cleaner
    for us to return an error rather than panic.

--- rust/src/failpoint_bridge.rs
+++ rust/src/failpoint_bridge.rs
@@ -1,8 +1,16 @@
 //! C++ bindings for the failpoint crate.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+use anyhow::Result;
+
 /// Expose the `fail::fail_point` macro to C++.
-pub fn failpoint(p: &str) {
+pub fn failpoint(p: &str) -> Result<()> {
     ostree_ext::glib::g_debug!("rpm-ostree", "{}", p);
-    fail::fail_point!(p);
+    fail::fail_point!(p, |r| {
+        Err(match r {
+            Some(ref msg) => anyhow::anyhow!("{}", msg),
+            None => anyhow::anyhow!("failpoint {}", p),
+        })
+    });
+    Ok(())
 }
