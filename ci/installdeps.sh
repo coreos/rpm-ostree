abe342e956383cf280cde9105603c6b71366b405
Jonathan Lebon <jonathan@jlebon.com>
2021-10-25 11:19:35 -0400

    Make `jq` a `BuildRequires`
    It's the only package which falls in the "not in spec file, but required
    for git builds" and it's tiny. So let's just shove it in the spec so it
    gets installed by the customary `dnf builddep`.

--- ci/installdeps.sh
+++ ci/installdeps.sh
@@ -7,7 +7,7 @@ dn=$(dirname $0)
 . ${dn}/libbuild.sh
 
 if [ -z "${SKIP_INSTALLDEPS:-}" ] && test $(id -u) -eq 0; then
-    dnf -y install jq dnf-plugins-core
+    dnf -y install dnf-plugins-core
     # we have the canonical spec file handy so just builddep from that
     # XXX: use --allowerasing as a temporary hack to ease the migration to libmodulemd2
     time dnf builddep --spec -y packaging/rpm-ostree.spec.in --allowerasing
