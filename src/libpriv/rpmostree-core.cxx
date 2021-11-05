coreos/rpm-ostree

commit 11aeb825d2cbac36429e035aa9efbaa522b995dc

core: Handle RPM file colors more efficiently
We only ever need to handle RPM file colors on architectures that have
them. And even on those that do, we only need to decide between
differently colored RPM files, and not e.g. documentation files. So we
can skip adding those to the map of files added.

src/libpriv/rpmostree-core.cxx
@@ -2495,6 +2495,13 @@ handle_file_dispositions (RpmOstreeContext *self,
                           GCancellable *cancellable,
                           GError      **error)
 {
+  /* we deal with color similarly to librpm (compare with skipInstallFiles()) */
+  rpm_color_t ts_color = rpmtsColor (ts);
+  rpm_color_t ts_prefcolor = rpmtsPrefColor (ts);
+
+  if (ts_color == 0)
+    return TRUE; /* this architecture doesn't do colours; done */
+
   g_autoptr(GHashTable) pkgs_deleted = g_hash_table_new (g_direct_hash, g_direct_equal);
 
   /* note these entries are *not* canonicalized for ostree conventions */
@@ -2528,7 +2535,8 @@ handle_file_dispositions (RpmOstreeContext *self,
             {
               char *fn = g_strdup (rpmfiFN (fi));
               rpm_color_t color = rpmfiFColor (fi);
-              ht_insert_path_for_nevra (files_added, nevra, fn, GUINT_TO_POINTER (color));
+              if (color)
+                ht_insert_path_for_nevra (files_added, nevra, fn, GUINT_TO_POINTER (color));
             }
         }
     }
@@ -2540,17 +2548,13 @@ handle_file_dispositions (RpmOstreeContext *self,
   g_autoptr(GHashTable) files_skip_delete =
     g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
 
-  /* we deal with color similarly to librpm (compare with skipInstallFiles()) */
-  rpm_color_t ts_color = rpmtsColor (ts);
-  rpm_color_t ts_prefcolor = rpmtsPrefColor (ts);
-
   /* skip added files whose colors aren't in our rainbow */
   GLNX_HASH_TABLE_FOREACH_KV (files_added, const char*, nevra, GHashTable*, paths)
     {
       GLNX_HASH_TABLE_FOREACH_KV (paths, const char*, path, gpointer, colorp)
         {
           rpm_color_t color = GPOINTER_TO_UINT (colorp);
-          if (color && ts_color && !(ts_color & color))
+          if (!(ts_color & color))
             ht_insert_path_for_nevra (files_skip_add, nevra, canonicalize_rpmfi_path (path), NULL);
         }
     }
@@ -2581,6 +2585,8 @@ handle_file_dispositions (RpmOstreeContext *self,
             g_hash_table_add (files_skip_delete, g_strdup (fn));
 
           rpm_color_t color = (rpmfiFColor (fi) & ts_color);
+          if (!color)
+            continue;
 
           /* let's make the safe assumption that the color mess is only an issue for /usr */
           const char *fn_rel = fn + strspn (fn, "/");
@@ -2899,7 +2905,7 @@ delete_package_from_root (RpmOstreeContext *self,
             S_ISDIR (mode)))
         continue;
 
-      if (g_hash_table_contains (files_skip, fn))
+      if (files_skip && g_hash_table_contains (files_skip, fn))
         continue;
 
       g_assert (fn != NULL);

commit 750e5617dbdfbfdc2eda52dcf5d8e9f60effc6d3

core: Fix minor regression on colorless arches
In 11aeb825d2cb ("core: Handle RPM file colors more efficiently"), we
added an early exit in `handle_file_dispositions` on arches which don't
use colours. However, this is incorrect because that function does more
than just colour handling; it also handles files shared between RPMs.

Fix this and expand the function's docstring.

We have tests for this, but it passed through because CI runs on x86_64.
But this would matter or i686 or armhfp (which is relevant for Fedora
IoT).

src/libpriv/rpmostree-core.cxx
@@ -2503,10 +2503,14 @@ ht_insert_path_for_nevra (GHashTable *ht, const char *nevra, char *path, gpointe
 }
 
 /* This is a lighter version of calculations that librpm calls "file disposition".
- * Essentially, we determine which file removals/installations should be skipped. The librpm
- * functions and APIs for these are unfortunately private since they're just run as part of
- * rpmtsRun(). XXX: see if we can make the rpmfs APIs public, but we'd still need to support
- * RHEL/CentOS anyway. */
+ * Essentially, we determine which file removals/installations should be skipped. For
+ * example:
+ * - if removing a pkg owning a file shared with another pkg, the file should not be deleted
+ * - if adding a pkg owning a file that already exists in the base (or another added pkg),
+ *   and they are both "coloured", we need to pick the preferred one
+ *
+ * The librpm functions and APIs for these are unfortunately private since they're just run
+ * as part of rpmtsRun(). XXX: see if we can make the rpmfs APIs public. */
 static gboolean
 handle_file_dispositions (RpmOstreeContext *self,
                           int           tmprootfs_dfd,
@@ -2519,9 +2523,6 @@ handle_file_dispositions (RpmOstreeContext *self,
   /* we deal with color similarly to librpm (compare with skipInstallFiles()) */
   rpm_color_t ts_color = rpmtsColor (ts);
   rpm_color_t ts_prefcolor = rpmtsPrefColor (ts);
-
-  if (ts_color == 0)
-    return TRUE; /* this architecture doesn't do colours; done */
 
   g_autoptr(GHashTable) pkgs_deleted = g_hash_table_new (g_direct_hash, g_direct_equal);
 
@@ -2575,7 +2576,7 @@ handle_file_dispositions (RpmOstreeContext *self,
       GLNX_HASH_TABLE_FOREACH_KV (paths, const char*, path, gpointer, colorp)
         {
           rpm_color_t color = GPOINTER_TO_UINT (colorp);
-          if (!(ts_color & color))
+          if (ts_color && !(ts_color & color))
             ht_insert_path_for_nevra (files_skip_add, nevra, canonicalize_rpmfi_path (path), NULL);
         }
     }