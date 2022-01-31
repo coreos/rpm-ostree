8738aaaee516475aec88c3507afe254be97899bd
Colin Walters <walters@verbum.org>
2022-01-31 18:46:00 -0500

    Add a `Dockerfile.containerci`
    This parallels our cosa build (which does a VM flow) and instead
    directly overlays the production FCOS container image.
    
    I am sure we are going to be doing more of this type of thing
    as we adapt more of our CI to be really "container native".
    
    My immediate motivation is being able to turn this on in Prow.
    
    (That said...we do want to avoid building our binary twice, because
     it's a bit expensive.  I need to investigate
      https://docs.ci.openshift.org/docs/architecture/ci-operator/#building-artifacts
     perhaps?

---
+++ ci/prow/test-container.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -xeuo pipefail
+
+fatal() {
+    echo "$@" 1>&2
+    exit 1
+}
+
+origindir=/etc/rpm-ostree/origin.d
+mkdir -p "${origindir}"
+cat > "${origindir}/clienterror.yaml" << 'EOF'
+base-refspec: "foo/x86_64/bar"
+EOF
+if rpm-ostree ex rebuild 2>err.txt; then
+   fatal "did rebuild with base-refspec"
+fi
+
+echo ok
