dc63a249abe2d176de6918213407584732437149
Benno Rice <benno.rice@oracle.com>
2022-02-28 12:49:27 +1100

    Add a pass to ensure C/C++ code under `src` meets style.

--- ci/codestyle.sh
+++ ci/codestyle.sh
@@ -6,11 +6,26 @@ set -euo pipefail
 echo -n "checking for tabs... "
 (git grep -E '^	+' -- '*.[ch]' || true) > tabdamage.txt
 if test -s tabdamage.txt; then
-    echo "Error: tabs in .[ch] files:"
-    cat tabdamage.txt
+    echo "Error: tabs in .[ch] files:" 1>&2
+    cat tabdamage.txt 1>&2
     exit 1
 fi
 rm tabdamage.txt
+echo "ok"
+
+echo -n "checking clang-format... "
+rm -f clang-unformatted.txt
+git ls-files 'src/**.c' 'src/**.cxx' 'src/**.h' 'src/**.hpp' | while read f; do
+    if ! clang-format --Werror --ferror-limit=1 --dry-run "$f" > /dev/null 2>&1; then
+        echo "$f" >> clang-unformatted.txt
+    fi
+done
+if test -s clang-unformatted.txt; then
+    echo "Error: clang-format needed on these files:" 1>&2
+    cat clang-unformatted.txt 1>&2
+    exit 1
+fi
+rm -f clang-unformatted.txt
 echo "ok"
 
 echo -n "checking rustfmt... "
