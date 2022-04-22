fed96cbbff3d6ecd024da28bf875c3a75d969ba2
Jonathan Lebon <jonathan@jlebon.com>
2022-04-22 17:48:00 -0400

    rust/libdnf-sys: Bridge hy_split_nevra()
    We'll need this in an upcoming patch.

--- rust/libdnf-sys/cxx/libdnf.cxx
+++ rust/libdnf-sys/cxx/libdnf.cxx
@@ -18,6 +18,9 @@
  * Boston, MA 02111-1307, USA.
  */
 
+#include <glib-unix.h>
+
+#include "libdnf-sys/lib.rs.h"
 #include "libdnf.hpp"
 
 namespace dnfcxx
@@ -53,5 +56,26 @@ guint64
 dnf_repo_get_timestamp_generated (DnfRepo &repo)
 {
   return ::dnf_repo_get_timestamp_generated (&repo);
+}
+// XXX: dedupe with rpmostree_decompose_nevra
+Nevra
+hy_split_nevra (rust::Str nevra)
+{
+  g_autofree char *name = NULL;
+  int epoch;
+  g_autofree char *version = NULL;
+  g_autofree char *release = NULL;
+  g_autofree char *arch = NULL;
+
+  g_autofree char *nevra_c = g_strndup (nevra.data (), nevra.length ());
+  if (::hy_split_nevra (nevra_c, &name, &epoch, &version, &release, &arch) != 0)
+    throw std::runtime_error (std::string ("Failed to decompose NEVRA string: ") + nevra_c);
+
+  Nevra r = {
+    rust::String (name),    (guint64)epoch,      rust::String (version),
+    rust::String (release), rust::String (arch),
+  };
+  // the copy here *should* get elided
+  return r;
 }
 }
