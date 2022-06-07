979c2bf4272759b7291c1af16da90383f63c9379
Colin Walters <walters@verbum.org>
2022-06-07 11:53:56 -0400

    cliwrap/dracut: Don't intercept if we're running in systemd
    This should fix `kdump.service` which we test in FCOS at least.

--- rust/src/cliwrap/dracut.rs
+++ rust/src/cliwrap/dracut.rs
@@ -6,6 +6,11 @@ use crate::cliwrap::cliutil;
 
 /// Primary entrypoint to running our wrapped `dracut` handling.
 pub(crate) fn main(argv: &[&str]) -> Result<()> {
+    // At least kdump.service runs dracut to generate a separate initramfs.
+    // We need to continue supporting that.
+    if crate::utils::running_in_systemd() {
+        return cliutil::exec_real_binary("dracut", argv);
+    }
     eprintln!(
         "This system is rpm-ostree based; initramfs handling is
 integrated with the underlying ostree transaction mechanism.
