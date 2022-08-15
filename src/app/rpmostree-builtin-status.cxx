coreos/rpm-ostree

commit 76e55de42d1d8fbfad3a724eff5a3f6bb8c03d86

app/status: Reuse `print_values` to print rpmmd repos
That way, the knowledge of handling `TREE_RIGHT` and `TREE_BRANCH` to
print a list lives in just one place.

While we're here, just merge `print_commit` and `print_origin_repos`.
The only special thing here is that `print_values` doesn't print
anything if no values exist, so we print the commit checksum ourselves
if no repos need to be printed.

src/app/rpmostree-builtin-status.cxx
@@ -480,9 +480,11 @@ print_daemon_state (RPMOSTreeSysroot *sysroot_proxy, GCancellable *cancellable, 
   return TRUE;
 }
 
-/* Print the result of rpmostree_context_get_rpmmd_repo_commit_metadata() */
+/* Print the commit checksum, and optionally the rpm repodata injected from
+ * rpmostree_context_get_rpmmd_repo_commit_metadata() */
 static void
-print_origin_repos (gboolean host_endian, guint maxkeylen, GVariantDict *commit_meta)
+print_commit (const char *key, const char *checksum, gboolean host_endian, guint max_key_len,
+              GVariantDict *commit_meta)
 {
   g_autoptr (GVariant) reposdata = g_variant_dict_lookup_value (
       commit_meta, "rpmostree.rpmmd-repos", G_VARIANT_TYPE ("aa{sv}"));
@@ -491,9 +493,14 @@ print_origin_repos (gboolean host_endian, guint maxkeylen, GVariantDict *commit_
     return;
 
   const guint n = g_variant_n_children (reposdata);
-  if (n == 0)
-    return;
+  if (n == 0 || !opt_verbose)
+    {
+      /* no repos to print, so this is just a pure kv print */
+      rpmostree_print_kv (key, max_key_len, checksum);
+      return;
+    }
 
+  g_autoptr (GPtrArray) repos = g_ptr_array_new_with_free_func (g_free);
   for (guint i = 0; i < n; i++)
     {
       g_autoptr (GVariant) child = g_variant_get_child_value (reposdata, i);
@@ -511,19 +518,12 @@ print_origin_repos (gboolean host_endian, guint maxkeylen, GVariantDict *commit_
       if (!host_endian)
         ts = GUINT64_FROM_BE (ts);
       g_autofree char *timestamp_string = rpmostree_timestamp_str_from_unix_utc (ts);
-      g_print ("  %*s%s %s (%s)\n", maxkeylen + 2, " ",
-               libsd_special_glyph (i == (n - 1) ? TREE_RIGHT : TREE_BRANCH), id, timestamp_string);
+      g_ptr_array_add (repos, g_strdup_printf ("%s (%s)", id, timestamp_string));
     }
-}
+  g_ptr_array_add (repos, NULL);
 
-/* Print commit checksum, and then prints repos if in verbose mode. */
-static void
-print_commit (const char *key, const char *checksum, gboolean host_endian, guint max_key_len,
-              GVariantDict *commit_meta)
-{
-  rpmostree_print_kv (key, max_key_len, checksum);
-  if (opt_verbose)
-    print_origin_repos (host_endian, max_key_len, commit_meta);
+  /* NB: print_values takes care of printing the checksum for us */
+  print_values (key, max_key_len, (const char *const *)repos->pdata, NULL, FALSE, checksum);
 }
 
 static gboolean

commit 9dc5dcd0594d122172493695e31b0c63e7ea2e74

app/status: Fix printing commits without rpmmd-repos metadata
This tripped up libostree installed tests which parses it directly from
`rpm-ostree status`:

https://github.com/ostreedev/ostree/blob/4471c252a9c9/tests/kolainst/destructive/staged-deploy.sh#L104

We should change that to use `--json` instead, though it was useful in
this case. Add a test here instead that would've caught this.

Fixes: 76e55de4 ("app/status: Reuse `print_values` to print rpmmd
repos")

src/app/rpmostree-builtin-status.cxx
@@ -489,10 +489,7 @@ print_commit (const char *key, const char *checksum, gboolean host_endian, guint
   g_autoptr (GVariant) reposdata = g_variant_dict_lookup_value (
       commit_meta, "rpmostree.rpmmd-repos", G_VARIANT_TYPE ("aa{sv}"));
 
-  if (!reposdata)
-    return;
-
-  const guint n = g_variant_n_children (reposdata);
+  const guint n = reposdata ? g_variant_n_children (reposdata) : 0;
   if (n == 0 || !opt_verbose)
     {
       /* no repos to print, so this is just a pure kv print */

tests/vmcheck/test-misc-2.sh
@@ -26,6 +26,10 @@ set -x
 
 # More miscellaneous tests
 
+# Verify that the commit is printed in the output
+vm_rpmostree status > status.txt
+assert_file_has_content status.txt 'Commit:'
+
 # Locked finalization
 booted_csum=$(vm_get_booted_csum)
 commit=$(vm_cmd ostree commit -b vmcheck --tree=ref=vmcheck)