3526bb5c3793534fc9a7facb821fe506856a899c
Colin Walters <walters@verbum.org>
2022-10-05 19:33:34 -0400

    container: Enable wrappers duing transaction
    This ensures that we get the same wrappers for `/usr/bin/systemctl`
    and `/usr/bin/groupadd` in the container flow that we have
    at `compose tree` time.
    
    (Also, it's really confusing that this functionality is separate
     from `cliwrap`...we need to unify these two things)
    
    While I'm here, I hit an exciting bug in the container flow where
    I accidentally typo'd a package name, and because we didn't
    "undo on drop" we left our wrappers active.  (This doesn't matter
    on the compose side as we're always acting on a temporary filesystem)
    
    Bigger picture we should do
    https://internals.rust-lang.org/t/should-rust-programs-unwind-on-sigint/13800/11

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -217,7 +217,7 @@ pub mod ffi {
         fn undo(self: &TempEtcGuard) -> Result<()>;
 
         fn prepare_filesystem_script_prep(rootfs: i32) -> Result<Box<FilesystemScriptPrep>>;
-        fn undo(self: &FilesystemScriptPrep) -> Result<()>;
+        fn undo(self: &mut FilesystemScriptPrep) -> Result<()>;
 
         fn run_depmod(rootfs_dfd: i32, kver: &str, unified_core: bool) -> Result<()>;
 
