442d7a81bc8d47c647c25741b05ef296988e00e4
Colin Walters <walters@verbum.org>
2022-12-15 08:00:33 -0500

    upgrader: Use g_strdup() to match g_free()
    In practice, these are always the same, but the analyzer
    doesn't know that today.

--- src/daemon/rpmostree-sysroot-upgrader.cxx
+++ src/daemon/rpmostree-sysroot-upgrader.cxx
@@ -439,7 +439,7 @@ rpmostree_sysroot_upgrader_pull_base (RpmOstreeSysrootUpgrader *self, const char
         CXX_TRY_VAR (import,
                      rpmostreecxx::pull_container (*self->repo, *cancellable, r.refspec.c_str ()),
                      error);
-        new_base_rev = strdup (import->merge_commit.c_str ());
+        new_base_rev = g_strdup (import->merge_commit.c_str ());
         break;
       }
     case rpmostreecxx::RefspecType::Checksum:
