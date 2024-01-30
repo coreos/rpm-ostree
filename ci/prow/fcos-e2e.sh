a5046b7844bd4af2d568d5bd52ab561ca7f8f87f
Jonathan Lebon <jonathan@jlebon.com>
2024-01-30 10:02:45 -0500

    ci: Test `opt-usrlocal-overlays` end-to-end in Prow CI
    Since both Prow and CoreOS CI do a compose and run kola tests, we
    can change one of them to cover some different options without losing
    coverage on the default path.
    
    Follow-up to #4728.

--- ci/prow/fcos-e2e.sh
+++ ci/prow/fcos-e2e.sh
@@ -9,7 +9,12 @@ ls -al /usr/bin/rpm-ostree
 rpm-ostree --version
 cd $(mktemp -d)
 cosa init https://github.com/coreos/fedora-coreos-config/
+# let's turn on opt-usrlocal-overlays in this test since CoreOS CI already
+# covers the off path
+echo -e '\nopt-usrlocal-overlays: true\n' >> src/config/manifest.yaml
 cp /cosa/component-rpms/*.rpm overrides/rpm
+# XXX: temporarily import new ostree until it makes it into FCOS
+(cd overrides/rpm && curl -L --remote-name-all https://kojipkgs.fedoraproject.org//packages/ostree/2024.2/1.fc39/x86_64/ostree-{,libs-}2024.2-1.fc39.x86_64.rpm)
 cosa fetch
 cosa build
 cosa kola run 'ext.rpm-ostree.*'
