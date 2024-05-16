0ba1ebafce94418c80440a1ca09e725ca2a72ae1
Jonathan Lebon <jonathan@jlebon.com>
2024-05-16 10:17:37 -0400

    packaging: drop `.in` extension on `rpm-ostree.spec.in`
    There's no processing done on this spec file nor is it hooked to the
    buildsystem. Drop the `.in` extension.
    
    Motivated by the fact that `dnf builddep rpm-ostree.spec.in` in dnf5
    gets confused by the non `.spec` extension. And it doesn't have a
    `--spec` switch yet to disambiguate.
    
    Related: https://github.com/rpm-software-management/dnf5/pull/810

--- ci/installdeps.sh
+++ ci/installdeps.sh
@@ -10,7 +10,7 @@ if [ -z "${SKIP_INSTALLDEPS:-}" ] && test $(id -u) -eq 0; then
     dnf -y install dnf-plugins-core
     # we have the canonical spec file handy so just builddep from that
     # XXX: use --allowerasing as a temporary hack to ease the migration to libmodulemd2
-    time dnf builddep --spec -y packaging/rpm-ostree.spec.in --allowerasing
+    time dnf builddep --spec -y packaging/rpm-ostree.spec --allowerasing
 
     osid="$(. /etc/os-release && echo $ID)"
     if [ "${osid}" == centos ]; then
