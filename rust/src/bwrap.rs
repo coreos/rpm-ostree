5f623c7ac45fd776d2257e6a7a0d4cdbdaaee8c5
Colin Walters <walters@verbum.org>
2025-07-28 21:48:27 -0400

    Revert "rust/bwrap: don't swallow STDERR when running commands"
    This reverts commit f4aecb9b62a97eee8368ca8058b33325891cd62d.
    It causes ill-defined behavior with older glib, resulting in
    file descriptor mis-assignments.

--- rust/src/bwrap.rs
+++ rust/src/bwrap.rs
@@ -446,8 +446,6 @@ impl Bubblewrap {
 
     /// Execute the container.  This method uses the normal gtk-rs `Option<T>` for the cancellable.
     fn run_inner(&mut self, cancellable: Option<&gio::Cancellable>) -> Result<()> {
-        // Merge STDERR/STDOUT so we don't swallow STDERR during execution
-        self.launcher.set_flags(gio::SubprocessFlags::STDERR_MERGE);
         let (child, argv0) = self.spawn()?;
         child_wait_check(child, cancellable).context(argv0)?;
         Ok(())
