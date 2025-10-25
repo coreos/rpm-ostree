29d99986e09ae5d0a0216e93ddc170eafdae1d25
Joseph Marrero Corchado <jmarrero@redhat.com>
2025-10-25 08:24:09 -0400

    wip

--- ci/prow/fcos-e2e.sh
+++ ci/prow/fcos-e2e.sh
@@ -6,9 +6,20 @@ set -xeuo pipefail
 export COSA_SKIP_OVERLAY=1
 # And suppress depcheck since we didn't install via RPM
 export COSA_SUPPRESS_DEPCHECK=1
+
 ls -al /usr/bin/rpm-ostree
 rpm-ostree --version
 cd $(mktemp -d)
+
+# inside your CI job
+mkdir -p "$PWD/.state" "$PWD/.cache" "$PWD/.config"
+chmod 700 "$PWD/.state" "$PWD/.config"
+chmod 755 "$PWD/.cache"
+
+export XDG_STATE_HOME="$PWD/.state"
+export XDG_CACHE_HOME="$PWD/.cache"
+export XDG_CONFIG_HOME="$PWD/.config"
+
 cosa init https://github.com/coreos/fedora-coreos-config/
 cp /cosa/component-rpms/*.rpm overrides/rpm
 cosa fetch
