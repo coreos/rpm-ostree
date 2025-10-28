74ab4e93f235ce0126ff83f2aabc2edc48317ec5
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-28 12:16:58 -0700

    fix: always pull filelists
    https://fedoraproject.org/wiki/Changes/DNFConditionalFilelists

--- src/libpriv/rpmostree-core.cxx
+++ src/libpriv/rpmostree-core.cxx
@@ -223,6 +223,9 @@ rpmostree_context_new_base (OstreeRepo *repo)
   /* Force disable internal libdnf Count Me logic */
   dnf_conf_add_setopt ("*.countme", DNF_CONF_COMMANDLINE, "false", NULL);
 
+  /* Always pull file lists */
+  dnf_conf_main_set_option ("optional_metadata_types", DNF_CONF_COMMANDLINE, "filelists", NULL);
+
   /* Hack until libdnf+librepo know how to better negotaiate zchunk.
    * see also the bits in configure.ac that define HAVE_ZCHUNK
    **/
